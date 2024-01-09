d4b5a1c866ef9d221d3b9bf63d16512a662efb7b
me_madness <p_rangel@abv.bg>
2024-01-09 19:55:57 +0100

    05 is done by the lecture

--- Python Advanced/01 - Lists as Stacks and Queues/05. hot_patato.py
+++ Python Advanced/01 - Lists as Stacks and Queues/05. hot_patato.py
@@ -1,6 +1,16 @@
 # First task from the lecture
 
+from collections import deque
 
+names = deque(input()).split()
+n_toss = int(input()) -1
+
+while len(names) > 1:
+    names.rotate(-n_toss)
+    removed_kid = names.popleft()
+    print(f"Removed {removed_kid}")
+
+print(f"Last is {removed_kid}")
 
 
 
