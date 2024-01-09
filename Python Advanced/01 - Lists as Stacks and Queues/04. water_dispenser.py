7a57dec67d90e33bc3ef8f0fea46c0afb1d5cce2
me_madness <p_rangel@abv.bg>
2024-01-09 18:56:39 +0100

    begin task 03

--- Python Advanced/01 - Lists as Stacks and Queues/04. water_dispenser.py
+++ Python Advanced/01 - Lists as Stacks and Queues/04. water_dispenser.py
@@ -1,6 +1,16 @@
 # First task from the lecture
+from collections import deque
 
+name = input()
+customers = deque()
 
-
-
+while name != "End":
+    if name == "Paid":
+        pass
+    else:
+        customers.append(name)
+    name = input()
+    
+    
+    
 # Second task from me
