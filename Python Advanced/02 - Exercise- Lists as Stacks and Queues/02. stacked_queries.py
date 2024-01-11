d21071536ee0627e167818dfc84c7b23e5c62625
me_madness <p_rangel@abv.bg>
2024-01-11 20:49:41 +0100

    03  is done by the lecture

--- Python Advanced/02 - Exercise- Lists as Stacks and Queues/02. stacked_queries.py
+++ Python Advanced/02 - Exercise- Lists as Stacks and Queues/02. stacked_queries.py
@@ -22,7 +22,28 @@ numbers.reverse()
 
 print(*numbers, sep=", ")
 
+# One more task from the lecture
 
+numbers = []
+
+def sum(a, b):
+    return  a + b
+
+map_functions = {
+    "1": lambda x: numbers.append(x[1]),
+    "2": lambda x: numbers.pop() if numbers else None,
+    "3": lambda x: print(max(numbers)) if numbers else None,
+    "4": lambda x: print(min(numbers)) if numbers else None,
+}
+
+for _ in range(int(input())):
+    numbers_data = input().split()  # "1 97" -> ["1", "97"] -> [1, 97]
+    command = numbers_data[0]  # 1
+    map_functions[command](numbers_data)
+
+numbers.reverse()
+
+print(*numbers, sep=", ")
 
 # Second task from me
 
