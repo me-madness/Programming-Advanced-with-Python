328f3fc5fdf649ffc26775f88bf38b467eb6eb53
me_madness <p_rangel@abv.bg>
2024-01-15 17:39:25 +0100

    03 is done by the lecture

--- Python Advanced/04 - Exercise Tuples and Sets/03. periodic_table.py
+++ Python Advanced/04 - Exercise Tuples and Sets/03. periodic_table.py
@@ -1,7 +1,12 @@
 # First task from the lecture
 
+table = set()
 
+for _ in range(int(input)):
+    for el in input().split():
+        table.add(el)
 
+print(*table, sep="\n")
 
 
 # Second task from me
