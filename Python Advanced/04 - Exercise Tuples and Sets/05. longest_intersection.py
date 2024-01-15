734b504883f28112d25f706220e39fe45a3e59bc
me_madness <p_rangel@abv.bg>
2024-01-15 18:23:43 +0100

    05 is begin from the lecture

--- Python Advanced/04 - Exercise Tuples and Sets/05. longest_intersection.py
+++ Python Advanced/04 - Exercise Tuples and Sets/05. longest_intersection.py
@@ -1,8 +1,9 @@
 # First task from the lecture
 
+longest_intersection = set()
 
-
-
+for _ in range(int(input()))
+    first_data, second_data = [el.split(",") for el in input().split("-")]
 
 # Second task from me
 
