d23a6ed19204297c65fce562a4c384e170fe1c97
me_madness <p_rangel@abv.bg>
2024-01-15 18:29:44 +0100

    first way from the lecture is done

--- Python Advanced/04 - Exercise Tuples and Sets/05. longest_intersection.py
+++ Python Advanced/04 - Exercise Tuples and Sets/05. longest_intersection.py
@@ -2,7 +2,7 @@
 
 longest_intersection = set()
 
-for _ in range(int(input()))
+for _ in range(int(input())):
     first_data, second_data = [el.split(",") for el in input().split("-")]
 
     first_set = set(range(int(first_data[0]), int(first_data[1]) + 1))
