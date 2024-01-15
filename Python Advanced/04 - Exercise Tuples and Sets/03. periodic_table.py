cf4b863f4d480900f04a47cb34562a694f6f10e6
me_madness <p_rangel@abv.bg>
2024-01-15 17:50:02 +0100

    one more way from the lecture

--- Python Advanced/04 - Exercise Tuples and Sets/03. periodic_table.py
+++ Python Advanced/04 - Exercise Tuples and Sets/03. periodic_table.py
@@ -8,6 +8,9 @@ for _ in range(int(input)):
 
 print(*table, sep="\n")
 
+# Second way from he lecture
+
+print(*{el  for _ in range(int(input())) for el in input().split()}, sep="\n")
 
 # Second task from me
 
