86e338a6a7c981f4cc058fbe8dd36e78aa0f892e
me_madness <p_rangel@abv.bg>
2024-01-15 17:27:41 +0100

    write some code

--- Python Advanced/04 - Exercise Tuples and Sets/02. sets_of_elements.py
+++ Python Advanced/04 - Exercise Tuples and Sets/02. sets_of_elements.py
@@ -1,7 +1,9 @@
 # First task from the lecture
 
+n, m = [int(x) for x in input().split()]  # "4 3" -> ["4", "3"] -> [4, 3]
 
-
+first_set = {input() for _ in range(n)}
+second_set = {input() for _ in range(m)}
 
 
 # Second task from me
