65e48f4065b56c0cf0ea2c0130c292a13c927cf5
me_madness <p_rangel@abv.bg>
2024-01-15 18:07:48 +0100

    first way is ready

--- Python Advanced/04 - Exercise Tuples and Sets/04. count_symbols.py
+++ Python Advanced/04 - Exercise Tuples and Sets/04. count_symbols.py
@@ -2,9 +2,13 @@
 
 occurrences = {}
 
-for letter in input():
-    occurrences[letter] = occurrences.get(letter, 0) + 1
+for symbol in input():
+    occurrences[symbol] = occurrences.get(symbol, 0) + 1
 
 
+for letter, times in sorted(occurrences.items()):
+    print(f"{symbol}: {times} time/s")
+    
+    
 # Second task from me
 
