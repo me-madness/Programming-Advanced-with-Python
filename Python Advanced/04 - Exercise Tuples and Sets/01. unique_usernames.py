265f3492d03aac1d2b86a7ce73d20ea1056c7f72
me_madness <p_rangel@abv.bg>
2024-01-15 17:17:04 +0100

    first task is done of the lecture

--- Python Advanced/04 - Exercise Tuples and Sets/01. unique_usernames.py
+++ Python Advanced/04 - Exercise Tuples and Sets/01. unique_usernames.py
@@ -1,8 +1,12 @@
 # First task from the lecture
 
 names_count = int(input())
+names = set()
 
-
+for _ in range(names_count):
+    names.add(input())
+    
+print(*names, sep="\n")    
 
 # Second task from me
 
