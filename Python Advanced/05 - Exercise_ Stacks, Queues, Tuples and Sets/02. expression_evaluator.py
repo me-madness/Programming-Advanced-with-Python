059b709410de818e6f31002dc0b22ca41eb46adb
me_madness <p_rangel@abv.bg>
2024-01-16 18:24:48 +0100

    Third way from the lecture is done

--- Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/02. expression_evaluator.py
+++ Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/02. expression_evaluator.py
@@ -47,7 +47,6 @@ functions = {
     "+": lambda i: reduce(lambda a, b: a + b, map(int, expression[:i])),
 }     
 
-
 while idx < len(expression):
     element = expression[idx]
             
@@ -60,6 +59,30 @@ while idx < len(expression):
     
 print(floor(int(expression[0]))) 
 
+# Third way from the lecture
+
+from collections import deque
+from math import floor
+
+expression = deque(input().split())
+
+idx = 0
+
+while idx < len(expression):
+    element = expression[idx]
+    
+   
             
+    if element in "*/+-":
+        for _ in range(idx - 1):
+            expression.appendleft(eval(f"{expression.popleft()} {element} {expression.popleft()}"))
+            
+        del expression[1]    
+        idx = 1
+        
+    idx += 1
+    
+print(floor(int(expression[0])))  
+           
 # Second task from me
 
