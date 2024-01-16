aa2317f017795bb5cb9cb03200fdce4f37185505
me_madness <p_rangel@abv.bg>
2024-01-16 17:14:02 +0100

    begin with the first task from the lecture

--- Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/01. numbers.py
+++ Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/01. numbers.py
@@ -1,6 +1,10 @@
 # First task from the lecture
 
+first_set = set(int(x) for x in input().split())
+second_set = set(int(x) for x in input().split())
 
+for _ in range(int(input())):
+    first_command, second_command, *data = input().split()  #"Add First 1 2 3" -> ["Add", "First", "1", "2", "3"]
 
 
 
