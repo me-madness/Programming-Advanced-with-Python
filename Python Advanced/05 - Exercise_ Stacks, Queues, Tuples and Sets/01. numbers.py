f4bceaae42d1b747ee156bd4227fb4057f2cba81
me_madness <p_rangel@abv.bg>
2024-01-16 17:22:54 +0100

    first way is done by the lecture

--- Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/01. numbers.py
+++ Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/01. numbers.py
@@ -6,6 +6,23 @@ second_set = set(int(x) for x in input().split())
 for _ in range(int(input())):
     first_command, second_command, *data = input().split()  #"Add First 1 2 3" -> ["Add", "First", "1", "2", "3"]
 
+    command = first_command + " " + second_command
+    
+    if command == "Add First":
+        [first_set.add(int(el)) for el in data]
+    elif command == "Add Second":
+        [second_set.add(int(el)) for el in data]
+    elif command == "Remove First":
+        [first_set.discard(int(el)) for el in data]
+    elif command == "Remove Second":
+        [second_set.discard(int(el)) for el in data]
+    elif command == "Check Subset":
+        print(first_set.issubset(second_set) or second_set.issubset(first_set))
+        
+ 
+print(*sorted(first_set), sep=", ")        
+print(*sorted(second_set), sep=", ")        
+
 
 
 # Second task from me
