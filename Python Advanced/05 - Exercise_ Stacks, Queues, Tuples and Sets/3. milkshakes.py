40b8bb09707d054ba7fa2a8dca83a22c3538621d
me_madness <p_rangel@abv.bg>
2024-01-17 11:38:52 +0100

    all task are done by the lecture

--- Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/3. milkshakes.py
+++ Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/3. milkshakes.py
@@ -7,16 +7,17 @@ cups_of_milk = deque(int(x) for x in input().split(", "))
 
 milkshakes = 0
 
-while milkshakes != 5 and chocolates and cups_of_milk;
+while milkshakes != 5 and chocolates and cups_of_milk:
     chocolate = chocolates.pop()
     cup_of_milk  = cups_of_milk.popleft()
     
     if chocolate <= 0 and cup_of_milk <= 0:
         continue
     elif chocolate <= 0:
+        chocolates.appendleft(cup_of_milk)
         continue
     elif cup_of_milk <= 0:
-        chocolates.append(chocolate - 5)
+        chocolates.append(chocolate)
         continue
     
     if cup_of_milk == chocolate:
