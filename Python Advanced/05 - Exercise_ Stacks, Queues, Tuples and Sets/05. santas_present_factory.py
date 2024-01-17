818e480f7999f3e5ed5180c1ed208f44ebb9c861
me_madness <p_rangel@abv.bg>
2024-01-17 10:24:39 +0100

    preparing the output for task 05

--- Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/05. santas_present_factory.py
+++ Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/05. santas_present_factory.py
@@ -1,7 +1,12 @@
 # First task from the lecture
 
 
-
+print("The presents are crafted! Merry Christmas")
+print("No presents this Christmas")
+print(f"Materials left: {}, {}, {}")
+print(f"Magic left: {}, {}, {}")
+print(f"{}: {}")
+print(f"{}: {}")
 
 
 # Second task from me
