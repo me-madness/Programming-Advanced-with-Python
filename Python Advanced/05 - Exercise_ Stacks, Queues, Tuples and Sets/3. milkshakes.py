3d7403b2922bf22f5ae59a6cb998a5154a4e2bc7
me_madness <p_rangel@abv.bg>
2024-01-17 09:52:18 +0100

    04 is done by the lecture

--- Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/3. milkshakes.py
+++ Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/3. milkshakes.py
@@ -1,28 +1,7 @@
 # First task from the lecture
 
-from collections import deque
-
-bees = deque(int(x) for x in input().split())
-nectar = [int(x) for x in input().split()]
-symbol = deque(input().split())
-
-functions = {
-    "*": lambda a, b: a * b, 
-    "/": lambda a, b: a / b if b != 0 else 0, 
-    "*": lambda a, b: a * b, 
-    "*": lambda a, b: a * b, 
-}
 
 
-
-while bees and nectar:
-    curr_bee = bees.popleft()
-    curr_nectar = nectar.pop()
-    
-    if curr_nectar < curr_bee:
-        bees.appendleft(curr_bee)
-    else:
-        pass    
 
 
 # Second task from me
