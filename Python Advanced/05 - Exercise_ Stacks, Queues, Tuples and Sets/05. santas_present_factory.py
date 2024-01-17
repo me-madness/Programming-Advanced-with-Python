298506b81b88ae9eab1c3ebc00f3b486b893fdc6
me_madness <p_rangel@abv.bg>
2024-01-17 10:33:04 +0100

    begin with checks

--- Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/05. santas_present_factory.py
+++ Python Advanced/05 - Exercise_ Stacks, Queues, Tuples and Sets/05. santas_present_factory.py
@@ -1,12 +1,33 @@
 # First task from the lecture
 
+from collections import deque
 
+materials = [int(x) for x in input().split()]
+magic_levels = deque(int(x) for x in input().split())
+
+crafted = []
+presents = {
+    150: "Doll",
+    250: "Wooden train",
+    300: "Teddy bear",
+    400: "Bicycle",
+}
+
+while materials and magic_levels:
+    material = materials.pop() if magic_levels[0] or not materials[-1] else 0
+    magic_level = magic_levels.popleft() if material or not magic_levels[0] else 0
+    
+    if not magic_level:
+        continue
+
+
+    
 print("The presents are crafted! Merry Christmas")
 print("No presents this Christmas")
 print(f"Materials left: {}, {}, {}")
 print(f"Magic left: {}, {}, {}")
 print(f"{}: {}")
-print(f"{}: {}")
+
 
 
 # Second task from me
