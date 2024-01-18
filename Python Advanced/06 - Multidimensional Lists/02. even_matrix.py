6bc0eed70534e45167424311081a40f3761e697b
me_madness <p_rangel@abv.bg>
2024-01-18 18:29:24 +0100

    Second task is done by the lecture

--- Python Advanced/06 - Multidimensional Lists/02. even_matrix.py
+++ Python Advanced/06 - Multidimensional Lists/02. even_matrix.py
@@ -1,8 +1,26 @@
 # First task from the lecture
 
+row_count = int(input())
 
+matrix = []
 
+for i in range(row_count):
+    data = [int(el) for el in input().split(", ") if int(el) % 2 == 0]
+    matrix.append(data)
 
+print(matrix)
 
+# even_matrix = []
+
+# for row in matrix:
+#     sub_list = []
+#     for el in row:
+#         if el % 2 == 0:
+#             sub_list.append(el)
+#     even_matrix.append(sub_list)
+    
+    
+# print(even_matrix)
+    
 # Second task from me
 
