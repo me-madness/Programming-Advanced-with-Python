7190e3122b3a9ab5a515c487ffbdf9d37dc315ae
me_madness <p_rangel@abv.bg>
2024-01-18 17:26:59 +0100

    is done from the lecture

--- Python Advanced/06 - Multidimensional Lists/01. sum_matrix_elements.py
+++ Python Advanced/06 - Multidimensional Lists/01. sum_matrix_elements.py
@@ -4,9 +4,12 @@
 matrix = []
 
 for i in range(0, 3):
+    sub_list = []
     for j in range(0, 2):
-        
-
+        sub_list.append(0)
+    matrix.append(sub_list)
+    
+print(matrix)    
 
 # Second task from me
 
