66c793741a3d79395d92a823043faa744d8934bf
me_madness <p_rangel@abv.bg>
2024-01-18 17:40:04 +0100

    begin with task 01

--- Python Advanced/06 - Multidimensional Lists/01. sum_matrix_elements.py
+++ Python Advanced/06 - Multidimensional Lists/01. sum_matrix_elements.py
@@ -1,39 +1,8 @@
 # First task from the lecture
 
-# [[0, 0], [0, 0], [0, 0]] -> Output
+row, col = [int(el) for el in input().split(", ")]
 
-matrix = []
 
-for i in range(0, 3):
-    sub_list = []
-    for j in range(0, 2):
-        sub_list.append(0)
-    matrix.append(sub_list)
-    
-print(matrix)    
-
-# Second way from SoftUni
-
-matrix = []
-for i in range(3):
-    sub_list = []
-    for j in range(2):
-        matrix[i].append(0) 
-        
-        
-print(matrix)    
-
-# Same task another output
- 
-# [[1, 2, 3], [1, 2, 3], [1, 2, 3]] -> Output 
- 
-matrix = []
-for i in range(3):
-    matrix.append([])
-    for j in range(1, 4):
-        sub_list.append(j) 
-    matrix.append(sub_list)
-    
-print(matrix)    
+   
 # Second task from me
 
---
+++ Python Advanced/06 - Multidimensional Lists/multidimensional_lists.py
@@ -0,0 +1,36 @@
+# First task from the lecture
+# [[0, 0], [0, 0], [0, 0]] -> Output
+
+matrix = []
+
+for i in range(0, 3):
+    sub_list = []
+    for j in range(0, 2):
+        sub_list.append(0)
+    matrix.append(sub_list)
+    
+print(matrix)    
+
+# Second way from SoftUni
+# [[0, 0], [0, 0], [0, 0]] -> Output
+
+matrix = []
+for i in range(3):
+    sub_list = []
+    for j in range(2):
+        matrix[i].append(0) 
+        
+        
+print(matrix)    
+
+# Same task another output
+# [[1, 2, 3], [1, 2, 3], [1, 2, 3]] -> Output 
+ 
+matrix = []
+for i in range(3):
+    matrix.append([])
+    for j in range(1, 4):
+        sub_list.append(j) 
+    matrix.append(sub_list)
+    
+print(matrix)  
