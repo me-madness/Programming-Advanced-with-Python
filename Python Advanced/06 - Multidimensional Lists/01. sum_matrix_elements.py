a696b7a19d826683884600953618e36f3f8a14fb
me_madness <p_rangel@abv.bg>
2024-01-18 17:36:30 +0100

    all ways are done for first task

--- Python Advanced/06 - Multidimensional Lists/01. sum_matrix_elements.py
+++ Python Advanced/06 - Multidimensional Lists/01. sum_matrix_elements.py
@@ -16,11 +16,24 @@ print(matrix)
 
 matrix = []
 for i in range(3):
-    matrix.append([])
+    sub_list = []
     for j in range(2):
-        matrix[i].append(-0)
+        matrix[i].append(0) 
+        
         
 print(matrix)    
+
+# Same task another output
+ 
+# [[1, 2, 3], [1, 2, 3], [1, 2, 3]] -> Output 
+ 
+matrix = []
+for i in range(3):
+    matrix.append([])
+    for j in range(1, 4):
+        sub_list.append(j) 
+    matrix.append(sub_list)
     
+print(matrix)    
 # Second task from me
 
