7c19fc7e5e098d5120ade7d8d28f22410aadd521
me_madness <p_rangel@abv.bg>
2024-01-18 17:45:45 +0100

    first task is done

--- Python Advanced/06 - Multidimensional Lists/01. sum_matrix_elements.py
+++ Python Advanced/06 - Multidimensional Lists/01. sum_matrix_elements.py
@@ -2,7 +2,15 @@
 
 row, col = [int(el) for el in input().split(", ")]
 
-
-   
+total_amount = 0
+matrix = []
+for i in range(row):
+    row_data = [int(el) for el in input().split(", ")]
+    total_amount += sum(row_data) 
+    matrix.append(row_data)
+    
+print(total_amount)    
+print(matrix)    
+    
 # Second task from me
 
