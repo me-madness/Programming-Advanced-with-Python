dee7dbf2d041053dc249e29f9012ea176c33aee7
me_madness <p_rangel@abv.bg>
2024-01-18 19:42:21 +0100

    05 is done by the lecture

--- Python Advanced/06 - Multidimensional Lists/05. primary_diagonal.py
+++ Python Advanced/06 - Multidimensional Lists/05. primary_diagonal.py
@@ -1,6 +1,17 @@
 # First task from the lecture
 
+row = int(input())
 
+matrix = []
+
+for _ in range(row):
+    matrix.append([int(el) for el in input().split()])
+    
+   
+diagonal_sum = 0
+for row_index in range(row):
+    diagonal_sum += matrix[row_index][row_index]
+print(diagonal_sum)        
 
 
 
