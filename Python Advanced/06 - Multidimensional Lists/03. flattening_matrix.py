4bb9e17587415f5d9a3ad54c603ad11b4abb8139
me_madness <p_rangel@abv.bg>
2024-01-18 18:35:58 +0100

    03 is done from the lecture

--- Python Advanced/06 - Multidimensional Lists/03. flattening_matrix.py
+++ Python Advanced/06 - Multidimensional Lists/03. flattening_matrix.py
@@ -1,8 +1,21 @@
 # First task from the lecture
 
+row = int(input())
 
+flatten = []
 
+for i in range(row):
+    data = [int(el) for el in input().split(", ") if int(el) % 2 == 0]
+    flatten.extend(data)
+print(flatten)
 
+# flatten = []
+
+# for row in matrix:
+#     for el in row:
+#         flatten.append(el)
+      
+# print(flatten)
 
 # Second task from me
 
