a6f2e2a8287f523c667f547c8745235d5c9247f8
me_madness <p_rangel@abv.bg>
2024-01-19 18:12:12 +0100

    new commit of task 04

--- Python Advanced/07 - Exercise - _Multidimensional Lists/04. maximal_sum.py
+++ Python Advanced/07 - Exercise - _Multidimensional Lists/04. maximal_sum.py
@@ -1,6 +1,17 @@
 # First task from the lecture
 
+rows, cols = [int(x) for x in input().split()]
+matrix = [[int(x) for x in input().split()] for _ in range(rows)] 
 
+
+max_sum = float("-inf")
+biggest_matrix = []
+
+for row in range(rows - 2):
+    for col in range(cols - 2):
+        first_row = matrix[row][col:col + 3]
+        second_row = matrix[row][col:col + 3]
+        third_row = matrix[row][col:col + 3]
 
 
 
