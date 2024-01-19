e83fd24859d5b23e725dceaff7979ab4dd323477
me_madness <p_rangel@abv.bg>
2024-01-19 17:26:07 +0100

    second task is done by the lecture

--- Python Advanced/07 - Exercise - _Multidimensional Lists/02. diagonal_difference.py
+++ Python Advanced/07 - Exercise - _Multidimensional Lists/02. diagonal_difference.py
@@ -1,8 +1,27 @@
 # First task from the lecture
 
 num = int(input())
-matrix = [[for el in input().split(", ")] for _ in range(num)]
+matrix = [[int(el) for el in input().split()] for _ in range(num)]
 primary_sum, second_sum = 0, 0
+
+for i in range(num):
+    primary_sum +=matrix[i][i]
+    second_sum += matrix[i][num - i - 1]
+
+print(abs(primary_sum - second_sum))
+
+# Second way from the lecture
+
+n = int(input())
+primary_sum, second_sum = 0, 0
+
+for i in range(n):
+    line = [int(el) for el in input().split()]
+    primary_sum +=line[i][i]
+    second_sum += line[i][num - i - 1]
+
+print(abs(primary_sum - second_sum))
+
 
 
 # Second task from me
