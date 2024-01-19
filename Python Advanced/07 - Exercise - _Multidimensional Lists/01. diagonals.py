a22477ab4e42975c632269682fd7f33460ee6f99
me_madness <p_rangel@abv.bg>
2024-01-19 17:15:02 +0100

    new commit of first task

--- Python Advanced/07 - Exercise - _Multidimensional Lists/01. diagonals.py
+++ Python Advanced/07 - Exercise - _Multidimensional Lists/01. diagonals.py
@@ -4,6 +4,7 @@ n = int(input())
 
 matrix = [[int(el) for el in input().split(", ")] for _ in range(n)] # ["1, 2, 3, 4", "1, 2, 3"]
 primary = [matrix[r][r] for r in range(n)]
+second= [matrix[r][n - r - 1] for r in range(n)]
 
 
 
