e9697df303fac6d4560a326f6d3ce3b468307d2a
me_madness <p_rangel@abv.bg>
2024-01-19 18:28:24 +0100

    04 and  05 are done

--- Python Advanced/07 - Exercise - _Multidimensional Lists/05.  matrix_of_palindtomes.py
+++ Python Advanced/07 - Exercise - _Multidimensional Lists/05.  matrix_of_palindtomes.py
@@ -1,6 +1,14 @@
 # First task from the lecture
 
+rows, cols = [int(x) for x in input().split()]
 
+start = ord('a')
+
+for row in range(start, start + rows):
+    for col in range(row, row + cols):
+        print(chr(row), chr(col), chr(row), sep="", end="")
+        
+    print()   
 
 
 
