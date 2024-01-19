c54584c7c21c2e00a7f0374582181136670e3efa
me_madness <p_rangel@abv.bg>
2024-01-19 17:12:57 +0100

    begin with task 01

--- Python Advanced/07 - Exercise - _Multidimensional Lists/01. diagonals.py
+++ Python Advanced/07 - Exercise - _Multidimensional Lists/01. diagonals.py
@@ -1,6 +1,14 @@
 # First task from the lecture
 
+n = int(input())
 
+matrix = [[int(el) for el in input().split(", ")] for _ in range(n)] # ["1, 2, 3, 4", "1, 2, 3"]
+primary = [matrix[r][r] for r in range(n)]
+
+
+
+print(f"Primary diagonal: {}, {}, {}. Sum: {}")
+print(f"Second diagonal: {}, {}, {}. Sum: {}")
 
 
 
