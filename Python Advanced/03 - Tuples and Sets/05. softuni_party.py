1f56e6437804333eee5fd523337a1240ee35b67d
me_madness <p_rangel@abv.bg>
2024-01-21 16:48:18 +0100

    almost done the task

--- Python Advanced/03 - Tuples and Sets/05. softuni_party.py
+++ Python Advanced/03 - Tuples and Sets/05. softuni_party.py
@@ -8,7 +8,15 @@ for _ in range(n):
     code = input()
     codes.add(code)
 
+code = input()
+while code != "END":
+    if code in codes:
+        codes.remove(code)
+    code = input()
 
-
+print(len(codes)) 
+for code in codes:
+    print(code)
+            
 # Second task from me
 
