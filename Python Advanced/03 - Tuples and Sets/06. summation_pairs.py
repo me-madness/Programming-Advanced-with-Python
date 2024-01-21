975034ef41f2dc927fda11771ab9fca13f195939
me-madness <58656549+me-madness@users.noreply.github.com>
2024-01-21 19:34:03 +0100

    First way from softuni is done

--- Python Advanced/03 - Tuples and Sets/06. summation_pairs.py
+++ Python Advanced/03 - Tuples and Sets/06. summation_pairs.py
@@ -3,10 +3,32 @@
 numbers = list(map(int, input().split()))
 target = int(input())
 
+# First way from SOftUni
+
 for i in range(len(numbers)):
-    
+    for j in range(i + 1, len(numbers)):
+        if numbers[i] + numbers[j] == target:
+            print(f"{numbers[i] + {numbers[j]} = {target}}")
+
+
+for i in range(len(numbers)):
+    if numbers[i] == "":
+        continue
+    for j in range(i + 1, len(numbers)):
+        if numbers[j] == "":
+            continue
+        if numbers[i] + numbers[j] == target:
+            print(f"{numbers[i] + {numbers[j]} = {target}}")
+            numbers[i] = ""
+            numbers[j] = ""
+            break
+ 
+# Second way from SoftUni
 
 
 
+
+
+            
 # Second task from me
 
