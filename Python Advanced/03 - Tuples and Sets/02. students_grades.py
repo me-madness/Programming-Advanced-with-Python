10b399dfed939dea9f318e789bf209a96e2a15e0
me_madness <p_rangel@abv.bg>
2024-01-21 14:59:18 +0100

    new commit for the same task

--- Python Advanced/03 - Tuples and Sets/02. students_grades.py
+++ Python Advanced/03 - Tuples and Sets/02. students_grades.py
@@ -2,10 +2,20 @@
 
 n = int(input())
 
+student_grades = {}
+
 for _ in range(n):
+    name, grade_as_str = input().split()
+    grade = float(grade_as_str)
     
-
-
+    if name not in student_grades:
+       student_grades[name] = []
+    student_grades[name].append(grade)    
+    
+for name, grades in student_grades.items():
+    avg_grade = sum(grades) / len(grades)
+    formatted_grades = f"{' '.join(grades)}"
+    print(f"{name} -> {formatted_grades} (avg: {avg_grade:.2f})")
 
 # Second task from me
 
