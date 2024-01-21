1a6f740dc4e7dd66ec059730e878a1ed8273803a
me-madness <58656549+me-madness@users.noreply.github.com>
2024-01-21 19:47:11 +0100

    Third way is also done

--- Python Advanced/03 - Tuples and Sets/06. summation_pairs.py
+++ Python Advanced/03 - Tuples and Sets/06. summation_pairs.py
@@ -1,15 +1,14 @@
 # First task from the lecture
 
+# First way from SOftUni
+
 numbers = list(map(int, input().split()))
 target = int(input())
-
-# First way from SOftUni
 
 for i in range(len(numbers)):
     for j in range(i + 1, len(numbers)):
         if numbers[i] + numbers[j] == target:
             print(f"{numbers[i] + {numbers[j]} = {target}}")
-
 
 for i in range(len(numbers)):
     if numbers[i] == "":
@@ -25,6 +24,9 @@ for i in range(len(numbers)):
  
 # Second way from SoftUni
 
+numbers = list(map(int, input().split()))
+target = int(input())
+
 targets = set()
 values_map = {}
 
@@ -32,7 +34,6 @@ for value in numbers:
     resulting_numbers = target - value
     targets.add(resulting_numbers)
     values_map[resulting_numbers] = value    
-
 
 for value in numbers:
     if value in targets:
@@ -45,8 +46,28 @@ for value in numbers:
         targets.add(resulting_numbers)
         values_map[resulting_numbers] = value    
 
+# Third way from softUni
 
+import time
 
+numbers = list(map(int, input().split()))
+target = int(input())
+
+start = time.time()
+targets = set()
+values_map = {}
+for value in numbers:
+    if value in targets:
+        targets.remove(value)
+        pair = values_map[value]
+        del values_map[value]
+        print(f"{pair} + {value} = {target}")
+    else:
+        resulting_numbers = target - value
+        targets.add(resulting_numbers)
+        values_map[resulting_numbers] = value 
+end = time.time()
+print(f"Time range: {end-start}")        
             
 # Second task from me
 
