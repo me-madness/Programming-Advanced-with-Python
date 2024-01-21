6d4d1131fcacfddb1e113504844fe597970b146f
me_madness <p_rangel@abv.bg>
2024-01-21 14:50:30 +0100

    Second way for ask one is also done

--- Python Advanced/03 - Tuples and Sets/01. count_same_values.py
+++ Python Advanced/03 - Tuples and Sets/01. count_same_values.py
@@ -4,12 +4,25 @@ numbers = tuple([float(el) for el in input().split()])
 
 same_values = {}
 
-
 for num in numbers:
     if num not in same_values:
         same_values[num] = numbers.count(num)
         
 for number, occ in same_values.items():
-    print(f"{number} - {occ} times")        
+    print(f"{number} - {occ} times")       
+     
+# Second way from the SoftUni
+
+numbers = tuple(map(float, input().split()))
+
+nums_and_occurances = {}
+
+for num in numbers:
+    if num not in nums_and_occurances:
+        nums_and_occurances[num] += 1
+    nums_and_occurances[num] += 1
+    
+[print(f"{key} - {value} times") for key, value in nums_and_occurances.items()]
+
 # Second task from me
 
