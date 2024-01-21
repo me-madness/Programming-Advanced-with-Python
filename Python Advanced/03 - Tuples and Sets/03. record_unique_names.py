a0c953f4a8dfc502ce4616b0509604b904fbf8f2
me_madness <p_rangel@abv.bg>
2024-01-21 16:31:36 +0100

    third task are ready

--- Python Advanced/03 - Tuples and Sets/03. record_unique_names.py
+++ Python Advanced/03 - Tuples and Sets/03. record_unique_names.py
@@ -1,8 +1,15 @@
 # First task from the lecture
 
+n = int(input())
 
+names = set()
 
+for _ in range(n):
+    name = input()
+    names.add(name)
 
+for name in names:
+    print(name)
 
 # Second task from me
 
