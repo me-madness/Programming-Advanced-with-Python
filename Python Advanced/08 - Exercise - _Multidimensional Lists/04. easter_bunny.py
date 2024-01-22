8bfad9d507c3f4183cb394d442cfb0bbf7aa7eb1
me_madness <p_rangel@abv.bg>
2024-01-22 18:16:49 +0100

    new changes

--- Python Advanced/08 - Exercise - _Multidimensional Lists/04. easter_bunny.py
+++ Python Advanced/08 - Exercise - _Multidimensional Lists/04. easter_bunny.py
@@ -9,5 +9,14 @@ best_path = []
 best_direction = None
 max_collected_eggs = float("-inf")
 
+direction = {
+    "up": (-1, 0),
+    "down": ()1, 0,
+    "left": (0, -1),
+    "right": (0, 1),
+}
+
+
+
 # Second task from me
 
