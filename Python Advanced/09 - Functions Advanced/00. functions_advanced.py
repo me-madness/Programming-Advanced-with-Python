568ab88587cd65da5bf65c283b44f6974c7f4f48
me_madness <p_rangel@abv.bg>
2024-01-23 17:23:23 +0100

    Done with first task

--- Python Advanced/09 - Functions Advanced/00. functions_advanced.py
+++ Python Advanced/09 - Functions Advanced/00. functions_advanced.py
@@ -1,8 +1,8 @@
-def sum_nums(*args):
-    total_sum = 0
+def sum_nums(a, b , *args):
+    total_sum = a + b
     for el in args:
         total_sum += el
     return total_sum
 
 
-print(sum_nums(*args: 3, 7, 10, 15))
+print(sum_nums( a = 5, b = 7, *args: 3, 7, 10, 15))
--- Python Advanced/09 - Functions Advanced/01. multiplication_functinon.py
+++ Python Advanced/09 - Functions Advanced/01. multiplication_functinon.py
@@ -1,7 +1,15 @@
 # First task is from the lecture
 
+def multiply(*args):
+    result = 1
+    for el in args:
+        result *= el
+    return result    
 
 
+print(multiply( 1, 4, 5))
+print(multiply(4, 5, 6, 1, 3))
+print(multiply(2, 0, 1000, 5000))
 
 # Second task is from me
 
