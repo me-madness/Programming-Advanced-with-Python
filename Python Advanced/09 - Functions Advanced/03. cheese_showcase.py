1a7aad9b1f10f4fdc242a81236c51ae672d865e0
me_madness <p_rangel@abv.bg>
2024-01-23 18:12:59 +0100

    03 small changes

--- Python Advanced/09 - Functions Advanced/03. cheese_showcase.py
+++ Python Advanced/09 - Functions Advanced/03. cheese_showcase.py
@@ -4,7 +4,7 @@ def sorting_cheeses(**kwargs):
     result = ""
     sorted_result = sorted(kwargs.items(), key=lambda kvp: (-len(kvp[1]), kvp[0]))
     
-    for cheese_name, quantities in resul:
+    for cheese_name, quantities in sorted_result:
         print(cheese_name)
         for quantity in sorted( quantities, reverse=True):
             result += f"{quantity} \n"
