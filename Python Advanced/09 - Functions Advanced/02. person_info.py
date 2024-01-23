a4be40364f8c33910d2a039f8d0e47d90f7ccb0f
me_madness <p_rangel@abv.bg>
2024-01-23 17:41:45 +0100

    begin with next task

--- Python Advanced/09 - Functions Advanced/02. person_info.py
+++ Python Advanced/09 - Functions Advanced/02. person_info.py
@@ -1,7 +1,7 @@
 # First task is from the lecture
 
 def get_info(name, town, age):
-    print(f"This is {name} from  {town} and he is {age} ")
+    print(f"This is {name} from  {town} and he is {age} years old")
     
     
 print(get_info(**{"name": "George", "town": "Sofiq", "age": 20}))    
