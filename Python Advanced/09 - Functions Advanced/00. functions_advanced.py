dfabb86a2366b9926f3fd97fb023d6803ac26b40
me_madness <p_rangel@abv.bg>
2024-01-23 17:30:38 +0100

    practice

--- Python Advanced/09 - Functions Advanced/00. functions_advanced.py
+++ Python Advanced/09 - Functions Advanced/00. functions_advanced.py
@@ -1,3 +1,5 @@
+# Practice
+
 def sum_nums(a, b , *args):
     total_sum = a + b
     for el in args:
@@ -5,4 +7,25 @@ def sum_nums(a, b , *args):
     return total_sum
 
 
-print(sum_nums( a = 5, b = 7, *args: 3, 7, 10, 15))
+print(sum_nums( a = 5, b = 7))
+
+# Practice
+
+def sum_nums(a, b ):
+    print(f"a is {a} and b is {b}")
+    
+    
+sum_nums(a = 1, b = 2)    
+
+# Practice
+
+def greet_me(**kwargs):
+    for key, value, in kwargs.items():
+        print(f"{value}, {key}")
+        
+        
+greet_me(a = 5, b = 6, c =7, d = 8,)     
+
+# Practice
+
+   
