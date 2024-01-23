c76c7a94058dbc55af706781ca6d6ea7e2f6337b
me_madness <p_rangel@abv.bg>
2024-01-23 18:12:21 +0100

    03 is done

--- Python Advanced/09 - Functions Advanced/03. cheese_showcase.py
+++ Python Advanced/09 - Functions Advanced/03. cheese_showcase.py
@@ -1,7 +1,23 @@
 # First task is from the lecture
 
+def sorting_cheeses(**kwargs):
+    result = ""
+    sorted_result = sorted(kwargs.items(), key=lambda kvp: (-len(kvp[1]), kvp[0]))
+    
+    for cheese_name, quantities in resul:
+        print(cheese_name)
+        for quantity in sorted( quantities, reverse=True):
+            result += f"{quantity} \n"
+    return result        
 
 
+print(
+    sorting_cheeses(
+        Parmesan = [102, 120, 135],
+        Camembert = [100, 100, 105, 500, 430],
+        Mozzarella = [50, 125],
+    )
+)
 
 # Second task is from me
 
