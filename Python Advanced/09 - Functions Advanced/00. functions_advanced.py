c5d0401589efe6e0ae364b497eb10e28e2db239a
me_madness <p_rangel@abv.bg>
2024-01-24 16:55:06 +0100

    new commit baby

--- Python Advanced/09 - Functions Advanced/00. functions_advanced.py
+++ Python Advanced/09 - Functions Advanced/00. functions_advanced.py
@@ -82,6 +82,14 @@ def greeting(name):
 
 # Practice Recursion - Function Calling Itself
 
+def factorial(number):
+    if not isinstance(number, int) or number < 0:
+        return f"Sorry. 'number' is incorrect."
 
+    def inner_factorial(n):
+        fact = 1
+        for i in range(1, n + 1):
+            fact = fact * i
+        return inner_factorial(number)    
 
 
