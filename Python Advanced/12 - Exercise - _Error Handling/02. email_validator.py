cdd3a1c02727431be47f21eb52ed9896bcb40b43
me_madness <p_rangel@abv.bg>
2024-01-29 18:12:13 +0100

    first error is write

--- Python Advanced/12 - Exercise - _Error Handling/02. email_validator.py
+++ Python Advanced/12 - Exercise - _Error Handling/02. email_validator.py
@@ -19,6 +19,18 @@ class InvalidNameError(Exception):
 VALID_DOMAINS = (".com", ".bg", ".org", ".net", ) 
 MIN_NAME_SYMBOLS_COUNT = 4
 
+email = input()
+
+while email != "End":
+    
+    if email.count("@") > 1:
+        raise MoreThanOneAtSymbol("Email should contain only one @ symbol!")
+    elif "@" not in email:
+        raise MoreThanOneAtSymbol("Email must contain @!")
+    
+    
+    email = input()
+
     
 # Second task from me
 
