30c064bb084d8119f59fb37ac89bcc2543d74a1a
me_madness <p_rangel@abv.bg>
2024-01-29 17:39:00 +0100

    first commit

--- Python Advanced/12 - Exercise - _Error Handling/01. numbers_dictionary.py
+++ Python Advanced/12 - Exercise - _Error Handling/01. numbers_dictionary.py
@@ -1,6 +1,15 @@
 # First task from the lecture
 
+numbers_dictionary = {}
 
+line = input()
+
+while line != "Search":
+    numbers_as_string = line
+    number = int(input())
+    numbers_dictionary[numbers_as_string] = number
+    
+line = input()    
 
 
 # Second task from me
