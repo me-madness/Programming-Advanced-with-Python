4b97c739eb5fdbee89783778a2c60bf0c236e581
me_madness <p_rangel@abv.bg>
2024-01-29 18:17:50 +0100

    two more check are done

--- Python Advanced/12 - Exercise - _Error Handling/02. email_validator.py
+++ Python Advanced/12 - Exercise - _Error Handling/02. email_validator.py
@@ -27,6 +27,11 @@ while email != "End":
         raise MoreThanOneAtSymbol("Email should contain only one @ symbol!")
     elif "@" not in email:
         raise MoreThanOneAtSymbol("Email must contain @!")
+    elif len(email.split("@")[0]) <= MIN_NAME_SYMBOLS_COUNT:
+        raise NameTooShortError("Name must be more than 4 characters!")
+    elif email.split(".")[-1] not in VALID_DOMAINS:    
+        raise InvalidDomainError(f"Domain must be one of the following: .com, .bg, .org, .net")
+    
     
     
     email = input()
