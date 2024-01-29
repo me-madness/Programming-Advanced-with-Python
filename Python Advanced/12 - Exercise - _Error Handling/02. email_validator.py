81f992f80fd90d292da68d82019e860eabcd8ae4
me_madness <p_rangel@abv.bg>
2024-01-29 18:07:23 +0100

    begin with second task

--- Python Advanced/12 - Exercise - _Error Handling/02. email_validator.py
+++ Python Advanced/12 - Exercise - _Error Handling/02. email_validator.py
@@ -1,8 +1,21 @@
 # First task from the lecture
 
+class NameTooShortError(Exception):
+    pass
 
+class MustContainAtSymbolError(Exception):
+    pass
 
+class InvalidDomainError(Exception):
+    pass
 
+class MoreThanOneAtSymbol(Exception):
+    pass
+
+class InvalidNameError(Exception):
+    pass
+    
+    
 # Second task from me
 
 
