8a0917b94b2e2b1c88e6e79c5a9f471e91dc440a
me_madness <p_rangel@abv.bg>
2024-01-30 17:46:25 +0100

    adding absolute path

--- Python Advanced/13 - File Handling/resources/00. main.py
+++ Python Advanced/13 - File Handling/resources/00. main.py
@@ -2,8 +2,12 @@
 
 import os
 
+ABSOLUTE_DIR_PATH = os.path.abspath(__file__)
 file_name = "text.txt"
 path = os.path.join("resources", file_name)
+
+
+
 try:
     file = open(file_name)
     print("File found")
