4ebd38c3d20ebeaf1e8ece2982b31c5bfa009109
me_madness <p_rangel@abv.bg>
2024-01-30 18:41:00 +0100

    start with task two

--- Python Advanced/13 - File Handling/01. file_opener.py
+++ Python Advanced/13 - File Handling/01. file_opener.py
@@ -1,6 +1,19 @@
 # First task from the lecture
 
+import os
 
+ABSOLUTE_DIR_PATH = os.path.abspath(__file__)
+file_name = "text.txt"
+path = os.path.join("resources", file_name)
+
+
+
+try:
+    file = open(file_name)
+    print("File found")
+except FileNotFoundError:
+    print("File is not found")
+    
 
 
 # Second task from me
--- Python Advanced/13 - File Handling/02. file_reader.py
+++ Python Advanced/13 - File Handling/02. file_reader.py
@@ -1,6 +1,10 @@
 # First task from the lecture
 
+file = open("my_example.txt")
 
+print(file.read(2))
+print(file.read(2))
+print(file.read(5))
 
 
 # Second task from me
