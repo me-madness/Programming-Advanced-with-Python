93555a35364f6b62f43565510fec06ea19466ab7
me_madness <p_rangel@abv.bg>
2024-01-30 19:21:42 +0100

    file deleting is done

--- Python Advanced/13 - File Handling/04. file_delete.py
+++ Python Advanced/13 - File Handling/04. file_delete.py
@@ -1,11 +1,22 @@
 # First task from the lecture
+# First option
 
 import os
 
 path = os.path.join("resources", "numbers.txt")
-os.remove(path)
 
-
+try:
+    os.remove(path)
+except FileNotFoundError:
+    print("The file is already deleted")
+    
+# Second option
+        
+if os.path.exists(path):
+    os.remove(path)
+else:
+    print("The file is already deleted")
+    
 # Second task from me
 
 
