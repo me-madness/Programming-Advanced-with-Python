157a0c05f94538fce9c41de2b7e68ecb74d2be1b
me_madness <p_rangel@abv.bg>
2024-01-30 18:45:26 +0100

    file reader

--- Python Advanced/13 - File Handling/02. file_reader.py
+++ Python Advanced/13 - File Handling/02. file_reader.py
@@ -5,7 +5,11 @@ file = open("my_example.txt")
 print(file.read(2))
 print(file.read(2))
 print(file.read(5))
+lines = file.readline()
+for line in lines:
+    print(line)
 
+[print(line, end="") for line in lines]
 
 # Second task from me
 
