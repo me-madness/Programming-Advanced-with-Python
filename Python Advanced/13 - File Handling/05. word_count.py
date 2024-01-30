496d0baa08373fd83f00ac2c2d16f7087f7081c5
me_madness <p_rangel@abv.bg>
2024-01-30 19:31:17 +0100

    05 word count

--- Python Advanced/13 - File Handling/05. word_count.py
+++ Python Advanced/13 - File Handling/05. word_count.py
@@ -1,6 +1,25 @@
 # First task from the lecture
 
+import os
+import re
 
+words_path = os.path.join("resources", "words.txt")
+text_path = os.path.join("resources", "input.txt")
+
+with open(words_path) as file:
+    searched_words_as_text = file.read()
+    searched_words = [word.lower() for word in searched_words_as_text.split()]
+    
+    
+with open(text_path) as file:
+    content = file.read().lower()
+    
+words_count = {}
+
+for searched_word in searched_words:
+    regex = re.compile(rf"\b{searched_word}")
+        
+"output.txt"
 
 
 # Second task from me
