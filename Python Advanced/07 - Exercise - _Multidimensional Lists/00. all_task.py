e666b3e0dd15e9f908d17b9beea1969af6c3cc2e
me_madness <p_rangel@abv.bg>
2024-01-31 19:40:55 +0100

    the next task are write

---
+++ Python Advanced/07 - Exercise - _Multidimensional Lists/00. all_task.py
@@ -0,0 +1,49 @@
+# 01. Diagonals
+
+
+
+# 02. Diagonal Difference
+
+
+
+
+# 03. 2x2 Squares in Matrix
+
+
+
+
+# 04. Maximal Sum
+
+
+
+
+# 05. matrix of Palindromes
+
+
+
+
+# 06. Matrix Shuffling
+
+
+
+
+# 07. Snake Moves
+
+
+
+
+# 08. Bombs
+
+
+
+
+# 09. Miner
+
+
+
+
+# 10. Radioactive Mutant Vampire Bunnies
+
+
+
+
