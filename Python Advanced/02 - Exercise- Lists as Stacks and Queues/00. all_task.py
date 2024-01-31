a02f7f98910c46eec042a314f0eb79f6cef286e4
me_madness <p_rangel@abv.bg>
2024-01-31 19:22:11 +0100

    all most done with writing the all task

---
+++ Python Advanced/02 - Exercise- Lists as Stacks and Queues/00. all_task.py
@@ -0,0 +1,30 @@
+# 01. Reverse Numbers
+
+
+
+# 02. Stacked Queries
+
+
+
+
+# 03. Fast Food
+
+
+
+
+# 04. Fashion Boutique
+
+
+
+
+# 05. Truck tour
+
+
+
+
+# 06. Balanced Parentheses
+
+
+
+
+# 07. Robotics
