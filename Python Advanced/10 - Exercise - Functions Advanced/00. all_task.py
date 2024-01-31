b60ea7a243cc5dfb77e824ff51a9c15b7e424a68
me_madness <p_rangel@abv.bg>
2024-01-31 19:50:43 +0100

    next task are done

---
+++ Python Advanced/10 - Exercise - Functions Advanced/00. all_task.py
@@ -0,0 +1,54 @@
+# 01. Negative vs Positive
+
+
+
+# 02. Keyword Arguments Length
+
+
+
+
+# 03. Even or Odd
+
+
+
+
+# 04. Numbers Filter
+
+
+
+
+# 05. Concatenate
+
+
+
+
+# 06. Function Executor
+
+
+
+
+# 07. Grocery
+
+
+
+
+# 08. Age Assignment
+
+
+
+
+# 09. Recursion Palindrome
+
+
+
+
+# 10. Fill the Box
+
+
+
+
+# 11. Math Operations
+
+
+
+
