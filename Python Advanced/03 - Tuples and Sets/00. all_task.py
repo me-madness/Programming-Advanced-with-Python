d98259dacc23608626077669a0330c97886a92e7
me_madness <p_rangel@abv.bg>
2024-01-31 19:25:57 +0100

    next task are write

---
+++ Python Advanced/03 - Tuples and Sets/00. all_task.py
@@ -0,0 +1,29 @@
+# 01. Count Same Values
+
+
+
+# 02. Students Grades
+
+
+
+
+# 03. Record Unique Names
+
+
+
+
+# 04. Parking Lot
+
+
+
+
+# 05. Softuni Party
+
+
+
+
+# 06. Summation Pairs
+
+
+
+
