53ee7ba9ea2cddd7d3832cf98a57dee18fa356f6
me_madness <p_rangel@abv.bg>
2024-01-31 19:43:41 +0100

    done with task in the new folder list

---
+++ Python Advanced/08 - Exercise - _Multidimensional Lists/00. all_task.py
@@ -0,0 +1,34 @@
+# 01. Flatten Lists
+
+
+
+# 02. Matrix Modification
+
+
+
+
+# 03. Knight Game
+
+
+
+
+# 04. Easter Bunny
+
+
+
+
+# 05. Alice in Wonderland
+
+
+
+
+# 06. Range Day
+
+
+
+
+# 07. Presents Delivery
+
+
+
+
