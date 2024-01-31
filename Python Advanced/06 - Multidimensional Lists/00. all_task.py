5dee1597391e85d85aae57133893dd534d8eb816
me_madness <p_rangel@abv.bg>
2024-01-31 19:36:45 +0100

    the task are in my new file

---
+++ Python Advanced/06 - Multidimensional Lists/00. all_task.py
@@ -0,0 +1,35 @@
+# 01. Sum Matrix Elements
+
+
+
+# 02. Even Matrix
+
+
+
+
+# 03. Flattening Matrix
+
+
+
+
+# 04. Sum matrix Columns
+
+
+
+
+# 05. Primary Diagonal
+
+
+
+
+# 06. Symbol in Matrix
+
+
+
+
+# 07. Square with Maximum
+
+
+
+
+# Multidimensional Lists
