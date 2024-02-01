90cd01ec87d10ed05c77fa3d161691ad428093a2
me_madness <p_rangel@abv.bg>
2024-02-01 18:25:36 +0100

    new commit for task 04

--- Python Advanced/14 - Exercise - File Handling/04. directory_traversal.py
+++ Python Advanced/14 - Exercise - File Handling/04. directory_traversal.py
@@ -6,5 +6,18 @@ def save_extensions(dir_name):
     for filename in os.listdir(dir_name):
         file = os.path.join(dir_name, filename)    
     
+        if os.path.isfile(file):
+            extension = filename.split(".")[-1]   
+    
+            if extension not in extensions:
+                extensions[extension] = []
+                
+                
+            extensions[extension].append()(filename)
+            
+            extensions.get(extension, []) + [filename]
+            
+            
+                
 directory = input()
 extensions = {}    
