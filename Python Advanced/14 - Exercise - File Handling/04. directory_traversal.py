cfdd51868c7676dfbe746a38937a6fb584867a5c
me_madness <p_rangel@abv.bg>
2024-02-01 18:26:58 +0100

    thats is it

--- Python Advanced/14 - Exercise - File Handling/04. directory_traversal.py
+++ Python Advanced/14 - Exercise - File Handling/04. directory_traversal.py
@@ -2,20 +2,16 @@
 
 import os
 
+
+
 def save_extensions(dir_name):
     for filename in os.listdir(dir_name):
         file = os.path.join(dir_name, filename)    
     
         if os.path.isfile(file):
             extension = filename.split(".")[-1]   
-    
-            if extension not in extensions:
-                extensions[extension] = []
-                
-                
-            extensions[extension].append()(filename)
             
-            extensions.get(extension, []) + [filename]
+            extensions[extension] = extensions.get(extension, []) + [filename]
             
             
                 
