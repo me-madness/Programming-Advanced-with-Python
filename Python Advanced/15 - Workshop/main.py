335bef6c2a19c7d3b25f7d051f4d188f64d3cc27
me_madness <p_rangel@abv.bg>
2024-02-02 17:14:30 +0100

    add some values

--- Python Advanced/15 - Workshop/main.py
+++ Python Advanced/15 - Workshop/main.py
@@ -12,7 +12,19 @@ board_map = []
 for _ in range(ROWS):
     board_map.append([0 for _ in range(COLS)])
     
-print_board()    
+turns = 1
+
+while True:
+    player = 2 if turns % 2 == 0 else 1
+    column = input(f"Player {player}, place chose a column")
+    column_index = int(column) - 1
+    
+    
+    turns += 1    
+    
+    
+    
+print_board(board_map)    
 
 
 
