0586c79d151e4ba22fa82892dc6dba36a5bf37b4
me_madness <p_rangel@abv.bg>
2024-02-03 11:53:40 +0100

    02. matching_ parentheses - is done

--- Python Advanced/01 - Lists as Stacks and Queues/00. all_task.py
+++ Python Advanced/01 - Lists as Stacks and Queues/00. all_task.py
@@ -1,14 +1,21 @@
 # 01. Reverse String
 
-reverse_string = input()
-new_string_reversed = reversed(reverse_string)
-print(''.join(new_string_reversed))
+# reverse_string = input()
+# new_string_reversed = reversed(reverse_string)
+# print(''.join(new_string_reversed))
 
 # 02. Matching Parentheses
 
+text = input()
+parentheses = []
 
-
-
+for i in range(len(text)):
+    if text[i] == "(":
+        parentheses.append(i)
+    elif text[i] == ")":
+        start_index = parentheses.pop()
+        print(text[start_index:i + 1])
+        
 # 03. Supermarket
 
 
