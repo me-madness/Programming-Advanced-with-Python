0eaaee65fa0a85cfbb5f05d08a35c794e11ab48f
me_madness <p_rangel@abv.bg>
2024-02-08 19:29:27 +0100

    new commit after the changes of the file authentication.py

--- Python Advanced/18 - Exercise - Modules/authentication.py
+++ Python Advanced/18 - Exercise - Modules/authentication.py
@@ -6,9 +6,9 @@ def render_entry():
     register_button = Button(
         root,
         text="Register",
-        bg="green",
-        fg="white", 
-        bd=0,
+        bg="green",        #background color
+        fg="white",        #color
+        bd=0,              #border
         width=90,
         height=40,
         command=register
@@ -31,6 +31,7 @@ def render_entry():
     )
     
     frame.create_window(350, 260, window=register_button)
+    frame.create_window(350, 310, window=login_button)
     
     
 def register():
