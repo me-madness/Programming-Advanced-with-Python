bf3b5f4d5c9ad759f6f49c3b3a00536e38069a94
me_madness <p_rangel@abv.bg>
2024-02-08 19:25:27 +0100

    adding a login button

--- Python Advanced/18 - Exercise - Modules/authentication.py
+++ Python Advanced/18 - Exercise - Modules/authentication.py
@@ -11,6 +11,31 @@ def render_entry():
         bd=0,
         width=90,
         height=40,
+        command=register
+        
+    )
+    
+
+    login_button = Button(
+        root,
+        text="Login",
+        bg="green",
+        fg="white", 
+        bd=0,
+        width=90,
+        height=40,
+        command=login
+        
+        
+        
     )
     
     frame.create_window(350, 260, window=register_button)
+    
+    
+def register():
+    print("Register")    
+    
+    
+def login():
+    print("Login")    
