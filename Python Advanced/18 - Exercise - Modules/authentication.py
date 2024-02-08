b84585e628e707bca70972f495b0298aa8ff18e1
me_madness <p_rangel@abv.bg>
2024-02-08 18:44:57 +0100

    import root

--- Python Advanced/18 - Exercise - Modules/authentication.py
+++ Python Advanced/18 - Exercise - Modules/authentication.py
@@ -1,5 +1,16 @@
 from tkinter import Button
+from canvas import root, frame
 
 
 def render_entry():
-    pass
+    register_button = Button(
+        root,
+        text="Register",
+        bg="green",
+        fg="white", 
+        bd=0,
+        width=90,
+        height=40,
+    )
+    
+    frame.create_window(350, 260, window=register_button)
--- Python Advanced/18 - Exercise - Modules/main.py
+++ Python Advanced/18 - Exercise - Modules/main.py
@@ -1,4 +1,6 @@
 from canvas import root
 
+
 if __name__ == "__main__":
+    render_entry()
     root.mainloop()
