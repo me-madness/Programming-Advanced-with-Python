12635a5479150fdb2dcca828af714a1117d537a2
me-madness <58656549+me-madness@users.noreply.github.com>
2024-02-12 20:23:21 +0100

    task 02 begin

--- Python Advanced/20 - Exam Preparation  - 12.02.2024/02. the_gambler.py
+++ Python Advanced/20 - Exam Preparation  - 12.02.2024/02. the_gambler.py
@@ -1,5 +1,37 @@
 # First task is from the lecture
 
+def is_in_boundaries(row_index, col_index, n):
+    if 0 <= row_index < n and 0 <
+
+
+
+
+
+
+direction_maper = {
+    "up": (-1, 0),
+    "down": (1, 0),
+    "left": (0, -1),
+    "right": (0, 1),
+}
+
+n  = int(input())
+
+board = []
+player_position = None
+
+for row_index in range(n):
+    data = list(input())
+    
+    if "G" in data:
+        col_index = data.index("G")
+        player_position = [row_index, col_index]
+
+
+
+
+
+
 
 
 # Second task is from me
