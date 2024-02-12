b35b5d4b5f277c35323d8d3e3ab4ffcca7d46440
me-madness <58656549+me-madness@users.noreply.github.com>
2024-02-12 17:13:32 +0100

    the task is almost done

--- Python Advanced/20 - Exam Preparation  - 12.02.2024/01. offroad_challenge.py
+++ Python Advanced/20 - Exam Preparation  - 12.02.2024/01. offroad_challenge.py
@@ -3,15 +3,22 @@
 from collections import deque
 
 initial_fuel = [int(el) for el in input().split()]
-additional_consumption_index = [int(el) for el in input().split()]
+additional_consumption_index = deque([int(el) for el in input().split()])
+amount_of_fuel_needed = deque([int(el) for el in input().split()])
 
+while initial_fuel and additional_consumption_index and amount_of_fuel_needed:
+    fuel = initial_fuel[-1]
+    index = additional_consumption_index[0]
+    needed = amount_of_fuel_needed[0]
 
-
-
-
-
-
-
+    if (fuel - index) >=  needed:
+        initial_fuel.pop()
+        additional_consumption_index.popleft()
+        amount_of_fuel_needed.popleft()
+    else:
+        
+        break    
+        
 
 
 
