2e0db50f6d0f72bf083b7920d6316a1f4399bd4d
me-madness <58656549+me-madness@users.noreply.github.com>
2024-02-12 17:08:08 +0100

    begin with task 01.

--- Python Advanced/20 - Exam Preparation  - 12.02.2024/01. offroad_challenge.py
+++ Python Advanced/20 - Exam Preparation  - 12.02.2024/01. offroad_challenge.py
@@ -1,5 +1,18 @@
 # First task is from the lecture
 
+from collections import deque
+
+initial_fuel = [int(el) for el in input().split()]
+additional_consumption_index = [int(el) for el in input().split()]
+
+
+
+
+
+
+
+
+
 
 
 # Second task is from me
