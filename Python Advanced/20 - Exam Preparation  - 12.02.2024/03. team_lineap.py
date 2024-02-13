e49eb356ecd38bb1d0d569e040eb2553093f39d6
me-madness <58656549+me-madness@users.noreply.github.com>
2024-02-13 20:07:49 +0100

    Update 03. team_lineap.py

--- Python Advanced/20 - Exam Preparation  - 12.02.2024/03. team_lineap.py
+++ Python Advanced/20 - Exam Preparation  - 12.02.2024/03. team_lineap.py
@@ -1,6 +1,23 @@
 # First task is from the lecture
 
 
+def team_lineup(*args):
+    country_player = {}
+    for player_name, country in args:
+        if country not in country_player:
+            country_player[country] = []
+        country_player[country].append(player_name)
+        
+    result = ""
+    
+    sorted_players = sorted(country_player.items(), key=lambda kvp: (-len(kvp[1]), kvp[0]))
+    for country, players in sorted_players:
+        result = ""        
+
+
+
+
+
 
 # Second task is from me
 
