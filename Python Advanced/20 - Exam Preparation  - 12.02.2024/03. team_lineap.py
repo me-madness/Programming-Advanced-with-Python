b94c0aa66b923a6d5373489c690b76baef583074
me-madness <58656549+me-madness@users.noreply.github.com>
2024-02-13 20:18:08 +0100

    team lineup is done

--- Python Advanced/20 - Exam Preparation  - 12.02.2024/03. team_lineap.py
+++ Python Advanced/20 - Exam Preparation  - 12.02.2024/03. team_lineap.py
@@ -12,11 +12,20 @@ def team_lineup(*args):
     
     sorted_players = sorted(country_player.items(), key=lambda kvp: (-len(kvp[1]), kvp[0]))
     for country, players in sorted_players:
-        result = ""        
+        result += f"{country}:\n"
+        for player_name in players:
+            result += f"  -{player_name}\n"
+    return result                
 
 
 
-
+print(team_lineup(
+    *args: ("Harry Kane", "England"),
+    ("Manuel Neuer", "Germany"),
+    ("Raheem Sterling", "England"),
+    ("Toni Kroos", "Germany"),
+    ("Cristiano Ronaldo", "Portugal"),
+    ("Thomas Muller", "Germany")))
 
 
 # Second task is from me
