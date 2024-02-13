89db11372cbd4c8765da97c30f3aebaf52547d6a
me_madness <p_rangel@abv.bg>
2024-02-13 18:34:40 +0100

    a middle of no ware with task 04

--- Python Advanced/21 - Algorithms Introduction/04. sum_of_coins.py
+++ Python Advanced/21 - Algorithms Introduction/04. sum_of_coins.py
@@ -1,7 +1,17 @@
 def choose_coins(coins, target):
     coins = coins.sort(reversed=True)
     index = 0
+    used_coins = {}   # coin: time_used
     
+    
+    while target > 0 and index < len(coins):
+        count_current_coins = target // coins[index]
+        target = target % coins[index]
+        
+        if count_current_coins > 0:
+            used_coins[coins[index]] = count_current_coins
+            
+        index += 1    
 
 
 
