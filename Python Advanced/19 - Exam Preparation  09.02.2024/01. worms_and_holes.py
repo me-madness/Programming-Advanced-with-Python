c7fc6b87e4aa50eb4b936079c7c03b22b75b559a
me_madness <p_rangel@abv.bg>
2024-02-16 18:13:23 +0100

    all most done

--- Python Advanced/19 - Exam Preparation  09.02.2024/01. worms_and_holes.py
+++ Python Advanced/19 - Exam Preparation  09.02.2024/01. worms_and_holes.py
@@ -1,2 +1,17 @@
+from collections import deque
+
 worms = [int(el) for el in input().split()]
-holes = [int(el) for el in input().split()]
+holes = deque([int(el) for el in input().split()])
+
+while worms and holes:
+    current_worm = worms[-1]
+    current_hole = holes[0]
+    
+    if current_worm == current_hole:
+        worms.pop()
+        holes.popleft()
+    else:
+        holes.popleft()
+        worms[-1] -= 3
+        if worms[-1] <= 0:
+            worms.pop()    
