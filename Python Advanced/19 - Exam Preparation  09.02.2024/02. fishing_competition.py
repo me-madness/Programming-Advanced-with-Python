5ff04d9e8f85a48983f133ae9828c5cd3bd07ab9
me_madness <p_rangel@abv.bg>
2024-02-16 20:05:13 +0100

    all most done with the task

--- Python Advanced/19 - Exam Preparation  09.02.2024/02. fishing_competition.py
+++ Python Advanced/19 - Exam Preparation  09.02.2024/02. fishing_competition.py
@@ -17,3 +17,13 @@ for row_index in range(n):
         current_col = data.index("S")
         position = [row_index, current_col]
     matrix.append(data)    
+    
+command = input()
+
+while command != "collect the nets":    
+    current_row_index, current_col_index = position
+    row_movement, col_movement = direction_mapper[command]
+    desired_row_index = current_row_index + row_movement
+    desired_col_index = current_col_index + col_movement
+    
+    command = input()
