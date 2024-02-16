351d2ccc6567fa8ee0941c2c973942e432275152
me_madness <p_rangel@abv.bg>
2024-02-16 18:21:45 +0100

    the task 01 is done

--- Python Advanced/19 - Exam Preparation  09.02.2024/01. worms_and_holes.py
+++ Python Advanced/19 - Exam Preparation  09.02.2024/01. worms_and_holes.py
@@ -3,6 +3,9 @@ from collections import deque
 worms = [int(el) for el in input().split()]
 holes = deque([int(el) for el in input().split()])
 
+all_worms_count = len(worms)
+count = 0
+
 while worms and holes:
     current_worm = worms[-1]
     current_hole = holes[0]
@@ -10,8 +13,27 @@ while worms and holes:
     if current_worm == current_hole:
         worms.pop()
         holes.popleft()
+        count += 1
     else:
         holes.popleft()
         worms[-1] -= 3
         if worms[-1] <= 0:
-            worms.pop()    
+            worms.pop()  
+
+
+if count:
+    print(f"Matches: {count}")              
+else:
+    print(f"There are no matches.")              
+                            
+if not worms and count == all_worms_count:
+    print(f"Every worm found a suitable hole!")              
+elif not worms and count < all_worms_count:                  
+    print(f"Worms left: none")              
+else:
+    print(f"Worms left: {', '.join([str(el) for el in worms])}")              
+                        
+if not holes:
+    print(f"Holes left: none")              
+else:                
+    print(f"Worms left: {', '.join([str(el) for el in holes])}")              
