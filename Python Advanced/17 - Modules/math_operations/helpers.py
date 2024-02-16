4b3fbf6398a5fa49c1f33b835f93c3e632ab1f24
me_madness <p_rangel@abv.bg>
2024-02-16 17:23:35 +0100

    math_operations is done

---
+++ Python Advanced/17 - Modules/math_operations/helpers.py
@@ -0,0 +1,9 @@
+from math_operations.core import add, subtract, divide, multiply, power
+
+sign_mapper = {
+    "+": add,
+    "-": subtract,
+    "/": divide,
+    "*": multiply,
+    "^": power
+}
