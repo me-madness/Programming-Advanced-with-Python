98e19fe28ab66a3fa7b993d208ac51f388a75dad
me_madness <p_rangel@abv.bg>
2024-02-16 19:44:06 +0100

    adding the direction mapper

--- Python Advanced/19 - Exam Preparation  09.02.2024/02. fishing_competition.py
+++ Python Advanced/19 - Exam Preparation  09.02.2024/02. fishing_competition.py
@@ -1,6 +1,14 @@
 n = int(input())
 
+direction_mapper = {
+    "up": (-1, 0),
+    "down": (1, 0),
+    "right": (0, 1),
+    "left": (0, -1),
+}
+
 matrix = []
+position = None
 
 for row_index in range(n):
     data = list(input())
