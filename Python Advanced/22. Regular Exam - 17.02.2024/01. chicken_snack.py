487c1c45de3dc3c62c63171dcf93ff97c5276c87
me_madness <p_rangel@abv.bg>
2024-02-17 13:22:40 +0100

    while loop is done

--- Python Advanced/22. Regular Exam - 17.02.2024/01. chicken_snack.py
+++ Python Advanced/22. Regular Exam - 17.02.2024/01. chicken_snack.py
@@ -4,12 +4,25 @@ money_in_pocket = [int(el) for el in input().split()]
 prices_of_foods = deque([int(el) for el in input().split()])
 
 food_count = 0
-rest_money = 0
 
 while money_in_pocket and prices_of_foods:
     current_money = money_in_pocket[-1]
     current_price = prices_of_foods[0]
     
+    if current_money > current_price:
+        rest_money = current_money - current_price
+        money_in_pocket.pop()
+        prices_of_foods.popleft()
+        money_in_pocket[-1] += rest_money
+        food_count +=1
+    elif current_money == current_price:
+        money_in_pocket.pop()
+        prices_of_foods.popleft()
+        food_count +=1
+    else:
+        money_in_pocket.pop()
+        prices_of_foods.popleft()   
+            
     
     
     
