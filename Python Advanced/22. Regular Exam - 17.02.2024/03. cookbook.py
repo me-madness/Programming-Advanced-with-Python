c0b4c30a1a287785268a354fe980a92d44bd85d0
me_madness <p_rangel@abv.bg>
2024-02-17 13:39:27 +0100

    begin with task 03. cookbook

---
+++ Python Advanced/22. Regular Exam - 17.02.2024/03. cookbook.py
@@ -0,0 +1,16 @@
+def cookbook():
+    pass
+
+
+
+
+
+
+
+print(cookbook(
+    ("Spaghetti Bolognese", "Italian", ["spaghetti", "tomato sauce", "ground beef"]),
+    ("Margherita Pizza", "Italian", ["pizza dough", "tomato sauce", "mozzarella"]),
+    ("Tiramisu", "Italian", ["ladyfingers", "mascarpone", "coffee"]),
+    ("Croissant", "French", ["flour", "butter", "yeast"]),
+    ("Ratatouille", "French", ["eggplant", "zucchini", "tomatoes"])
+))
