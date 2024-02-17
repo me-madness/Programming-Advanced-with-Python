286f8d7fa7e2131546369db5688a7cd16a7ab6f8
me_madness <p_rangel@abv.bg>
2024-02-17 16:07:26 +0100

    new commit of task 03

--- Python Advanced/22. Regular Exam - 17.02.2024/03. cookbook.py
+++ Python Advanced/22. Regular Exam - 17.02.2024/03. cookbook.py
@@ -12,31 +12,31 @@ def cookbook(*args):
     for country, cuisine_two in sorted_order_list:
         cuisine_count = len(cuisine_two) 
         result += f"{country} cuisine contains {cuisine_count} recipes:\n"
-        sorted_recipes = sorted(orders_list[country], key=lambda x: x[0])
+        sorted_recipes = sorted(orders_list[country], key=lambda kvp: (-len(kvp[1]), kvp[0]))
         for cuisine_name, ingredients_name in sorted_recipes:
             result += f" * {cuisine_name} -> Ingredients: {', '.join(el for el in ingredients_name)}\n"
     return result
-
-# print(cookbook(
-#     ("Spaghetti Bolognese", "Italian", ["spaghetti", "tomato sauce", "ground beef"]),
-#     ("Margherita Pizza", "Italian", ["pizza dough", "tomato sauce", "mozzarella"]),
-#     ("Tiramisu", "Italian", ["ladyfingers", "mascarpone", "coffee"]),
-#     ("Croissant", "French", ["flour", "butter", "yeast"]),
-#     ("Ratatouille", "French", ["eggplant", "zucchini", "tomatoes"])
-# ))
-
-# print(cookbook(
-#     ("Pad Thai", "Thai", ["rice noodles", "shrimp", "peanuts", "bean sprouts", "tamarind sauce"])
-#     ))
-
 
 print(cookbook(
     ("Spaghetti Bolognese", "Italian", ["spaghetti", "tomato sauce", "ground beef"]),
     ("Margherita Pizza", "Italian", ["pizza dough", "tomato sauce", "mozzarella"]),
     ("Tiramisu", "Italian", ["ladyfingers", "mascarpone", "coffee"]),
     ("Croissant", "French", ["flour", "butter", "yeast"]),
-    ("Ratatouille", "French", ["eggplant", "zucchini", "tomatoes"]),
-    ("Sushi Rolls", "Japanese", ["rice", "nori", "fish", "vegetables"]),
-    ("Miso Soup", "Japanese", ["tofu", "seaweed", "green onions"]),
-    ("Guacamole", "Mexican", ["avocado", "tomato", "onion", "lime"])
-    ))
+    ("Ratatouille", "French", ["eggplant", "zucchini", "tomatoes"])
+))
+
+# print(cookbook(
+#     ("Pad Thai", "Thai", ["rice noodles", "shrimp", "peanuts", "bean sprouts", "tamarind sauce"])
+#     ))
+
+
+# print(cookbook(
+#     ("Spaghetti Bolognese", "Italian", ["spaghetti", "tomato sauce", "ground beef"]),
+#     ("Margherita Pizza", "Italian", ["pizza dough", "tomato sauce", "mozzarella"]),
+#     ("Tiramisu", "Italian", ["ladyfingers", "mascarpone", "coffee"]),
+#     ("Croissant", "French", ["flour", "butter", "yeast"]),
+#     ("Ratatouille", "French", ["eggplant", "zucchini", "tomatoes"]),
+#     ("Sushi Rolls", "Japanese", ["rice", "nori", "fish", "vegetables"]),
+#     ("Miso Soup", "Japanese", ["tofu", "seaweed", "green onions"]),
+#     ("Guacamole", "Mexican", ["avocado", "tomato", "onion", "lime"])
+#     ))
