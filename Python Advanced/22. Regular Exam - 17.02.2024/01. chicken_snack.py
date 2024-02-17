56cc121722ec5ff2c7bcfb5c3bdb8c155fba6f50
me_madness <p_rangel@abv.bg>
2024-02-17 13:16:38 +0100

    the hole print are prepare

--- Python Advanced/22. Regular Exam - 17.02.2024/01. chicken_snack.py
+++ Python Advanced/22. Regular Exam - 17.02.2024/01. chicken_snack.py
@@ -4,6 +4,18 @@ money_in_pocket = [int(el) for el in input().split()]
 prices_of_foods = deque([int(el) for el in input().split()])
 
 food_count = 0
+rest_money = 0
 
 while money_in_pocket and prices_of_foods:
+    current_money = money_in_pocket[-1]
+    current_price = prices_of_foods[0]
     
+    
+    
+    
+    
+    
+print(f"Gluttony of the day! Henry ate {food_count} foods.")    
+print(f"Henry ate: {food_count} foods.")    
+print(f"Henry ate: {food_count} food.")    
+print(f"Henry remained hungry. He will try next weekend again.")    
