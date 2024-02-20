01547bf1c116c618c813e45f4ba000fa608c5fbc
me_madness <p_rangel@abv.bg>
2024-02-20 18:25:02 +0100

    create main.py file to testing my code

---
+++ Python OOP/01 - First Steps in OOP/main.py
@@ -0,0 +1,13 @@
+class Phone:
+    
+    def __init__(self, color, size):
+        self.color = color
+        self.size = size
+        
+        
+    def turn_on(self):
+        return 'The phone is turned on'    
+    
+
+p1 = Phone( color:"Green", size: 2)
+a = 5    
