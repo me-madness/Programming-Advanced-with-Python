c0c1904e94f240d06f8972ea5ae65b805a5dd8c4
me_madness <p_rangel@abv.bg>
2024-02-22 18:17:00 +0100

    task 05 flower is done

--- Python OOP/02 - Exercise - First Steps in OOP/05. flower.py
+++ Python OOP/02 - Exercise - First Steps in OOP/05. flower.py
@@ -1,17 +1,25 @@
 class Flower:
-    def __init__(self, name: str, water_requirements: int, is_happy: False) -> None:
+    def __init__(self, name: str, water_requirements: int) -> None:
         self.name = name
         self.water_requirements = water_requirements
-        self.is_happy = is_happy
+        self.is_happy = False
     
     
-    def water(self):
-        pass
+    def water(self, quantity: int) -> None:
+        if quantity >= self.water_requirements:
+            self.is_happy = True
     
     
     def status(self):
-        pass
-
+        # First option
+        return f"{self.name} is {'' if self.is_happy else 'not'}happy"
+        
+        ## Second option
+        # if self.is_happy:
+        #     return f"{self.name} is happy"
+        
+        # return f"{self.name} is not happy"
+    
     
 flower = Flower("Lilly", 100)
 flower.water(50)
