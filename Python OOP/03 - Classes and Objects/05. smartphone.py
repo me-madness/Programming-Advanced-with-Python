4d6a6950a21a114a02580e028895cc0812b79fe1
me_madness <p_rangel@abv.bg>
2024-02-23 19:39:10 +0100

    05 Smartphone is done

--- Python OOP/03 - Classes and Objects/05. smartphone.py
+++ Python OOP/03 - Classes and Objects/05. smartphone.py
@@ -11,12 +11,18 @@ class Smartphone:
         self.is_on = False    
     
     
-    def power(self):
-        pass
+    def install(self, app, app_memory):
+        if self.memory >= app_memory and self.is_on:
+            self.apps.append(app)
+            self.memory -= app_memory
+            return f"Installing {app}"
+        elif self.memory >= app_memory and not self.is_on:
+            return f"Turn on your phone to install {app}"
+            
+        return f"Not enough memory to install {app}"
     
-    
-    def power(self):
-        pass
+    def status(self):
+        return f"Total apps: {len(self.apps)}. Memory left: {self.memory}"
     
     
 smartphone = Smartphone(100)
