157534cdf7bfd748d9a555db84849050e96f1116
me_madness <p_rangel@abv.bg>
2024-02-23 18:07:34 +0100

    prepare the last task 05 Smartphone

---
+++ Python OOP/03 - Classes and Objects/05. smartphone.py
@@ -0,0 +1,24 @@
+class Smartphone:
+    def __init__(self) -> None:
+        pass
+    
+    
+    def power(self):
+        pass
+    
+    
+    def power(self):
+        pass
+    
+    
+    def power(self):
+        pass
+    
+    
+smartphone = Smartphone(100)
+print(smartphone.install("Facebook", 60))
+smartphone.power()
+print(smartphone.install("Facebook", 60))
+print(smartphone.install("Messenger", 20))
+print(smartphone.install("Instagram", 40))
+print(smartphone.status())    
