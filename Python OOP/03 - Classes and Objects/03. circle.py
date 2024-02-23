92a9b814a224e1edd08de85360ff2b02abc8b098
me_madness <p_rangel@abv.bg>
2024-02-23 18:05:56 +0100

    prepare task 04 Glass

--- Python OOP/03 - Classes and Objects/03. circle.py
+++ Python OOP/03 - Classes and Objects/03. circle.py
@@ -13,3 +13,9 @@ class Circle:
     
     def get_circumference(self):
         pass
+    
+    
+circle = Circle(10)
+circle.set_radius(12)    
+print(circle.get_area())
+print(circle.get_circumference())
---
+++ Python OOP/03 - Classes and Objects/04. glass.py
@@ -0,0 +1,23 @@
+class Glass:
+    def __init__(self) -> None:
+        pass
+    
+    
+    def fill(self):
+        pass
+
+
+    def empty(self):
+        pass
+    
+    
+    def info(self):
+        pass
+    
+
+glass = Glass()
+print(glass.fill(100))
+print(glass.fill(200))
+print(glass.empty())
+print(glass.fill(200))
+print(glass.info())    
