af8b545084bebcf884c0c6b8388585fec9300f35
me_madness <p_rangel@abv.bg>
2024-02-26 17:17:46 +0100

    begin with task 01. vet

---
+++ Python OOP/04 - Exercise - Classes and Objects/01. vet.py
@@ -0,0 +1,16 @@
+class Vet:
+    def __init__(self,name: str) -> None:
+        self.name = name
+        
+        
+    def register_animal(self, animal_name: str):
+        pass
+    
+    
+    def unregister_animal(self):
+        pass
+    
+    
+    def info(self):
+        pass
+        return f"{} has {} animals. {} space ;eft in vet."    
