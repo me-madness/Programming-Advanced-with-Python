7627ee709c59876eae7a4b6910b7bf582601e059
me_madness <p_rangel@abv.bg>
2024-02-26 17:26:26 +0100

    the task 01 Vet is done

--- Python OOP/04 - Exercise - Classes and Objects/01. vet.py
+++ Python OOP/04 - Exercise - Classes and Objects/01. vet.py
@@ -1,16 +1,31 @@
 class Vet:
-    def __init__(self,name: str) -> None:
-        self.name = name
+    animals: list[str] = []
+    space: int = 5     #SPACE: int = 5
+    
+    def __init__(self,vet_name: str) -> None:
+        self.name = vet_name
+        self.animals: List[str] = []
         
         
-    def register_animal(self, animal_name: str):
+    def register_animal(self, animal_name: str) -> str:
+        if len(Vet.animals) == Vet.space:
+            return f"Not enough space"
+        
+        self.animals.append(animal_name)
+        Vet.animals.append(animal_name)
+        
+        return f"{animal_name} registered in the clinic"
+    
+    
+    def unregister_animal(self, animal_name: str) -> str:
+        if animal_name not in self.animals:
+            return f"{animal_name} not in the clinic"
+        
+        self.animals.remove(animal_name)
+        Vet.animals.remove(animal_name)
+    
+        return f"{animal_name} unregistered successfully"
+    
+    def info(self) -> str:
         pass
-    
-    
-    def unregister_animal(self):
-        pass
-    
-    
-    def info(self):
-        pass
-        return f"{} has {} animals. {} space ;eft in vet."    
+        return f"{self.name} has {len(self.animals)} animals. {Vet.space - len(Vet.animals)} space ;eft in vet."    
