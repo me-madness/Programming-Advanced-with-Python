edc408812798223c3fddc33e1fb9364e83705493
me_madness <p_rangel@abv.bg>
2024-02-27 17:29:44 +0100

    new changes added in my Student class

--- Python OOP/05 - Inheritance/main.py
+++ Python OOP/05 - Inheritance/main.py
@@ -9,11 +9,29 @@ class Person:
 
 
 class Student(Person):
+    def __init__(self, first_name: str, last_name: str, fac_number: int) -> None:
+        super().__init__(first_name, last_name)
+        # This is the same 
+        # Person.__init__(self, first_name, last_name)
+        self.fac_number = fac_number
+    
+    
     def go_to_school(self):
         return f"Walking to school"
     
+    
+    def display_info(self):
+        return f"{self.get_ful_name()} with fac number {self.fac_number}"
+    
 
-person = Person("Test", "Testov")    
+person = Person("Test", "Testov", 213455)    
+student = Student("Test", "Testov", 213455)    
 print(person.first_name)    
 print(person.last_name)    
-print(person.get_ful_name)    
+print(person.get_ful_name) 
+print(student.first_name)    
+print(student.last_name)    
+print(student.fac_number)    
+print(student.get_ful_name)   
+print(student.go_to_school)   
+print(student.display_info)   
