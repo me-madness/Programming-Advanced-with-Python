dc775e753136e348a84ab80f3a261aed5895bbf7
me_madness <p_rangel@abv.bg>
2024-03-04 18:42:23 +0100

    05 Account is done

--- Python OOP/07 - Encapsulation/05. account.py
+++ Python OOP/07 - Encapsulation/05. account.py
@@ -16,3 +16,11 @@ class Account:
             self.__pin == new_pin
             return "Pin changed"
         return "Wrong pin"
+    
+    
+account = Account(8827312, 100, 3421)
+print(account.get_id(1111))
+print(account.get_id(3421))
+print(account.balance)
+print(account.change_pin(2212, 4321))
+print(account.change_pin(3421, 1234))    
