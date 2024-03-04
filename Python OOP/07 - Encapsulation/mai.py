f647a478f3ce0d3a2db3867c7213a7d918d68fb3
me_madness <p_rangel@abv.bg>
2024-03-04 17:15:06 +0100

    new commit in main.py

---
+++ Python OOP/07 - Encapsulation/mai.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, age):
+      self.name = name
+      self.age = age
+      
+      
+    @property
+    def age(self):
+        return self.__age
+    
+    
+    @age.setter
+    def age(self, value: int):
+        if value <= 0:
+            raise Exception("Age must be greater than zero")
+        self.__age = value  
