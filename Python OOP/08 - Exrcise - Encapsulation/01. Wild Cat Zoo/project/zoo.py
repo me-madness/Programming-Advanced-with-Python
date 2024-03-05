3c3ff1ff84f38a03bc8e368903f6b197a3cb5d94
me_madness <p_rangel@abv.bg>
2024-03-05 17:36:58 +0100

    all method are added to my file

--- Python OOP/08 - Exrcise - Encapsulation/01. Wild Cat Zoo/project/zoo.py
+++ Python OOP/08 - Exrcise - Encapsulation/01. Wild Cat Zoo/project/zoo.py
@@ -9,4 +9,30 @@ class Zoo:
       
       
     def add_animal(self, animal:str, price: int) -> str:
-          
+        pass
+    
+    
+    def hie_worker(self, worker: str) -> str:
+        pass
+    
+    
+    def fire_worker(self, worker_name: str) -> str:
+        pass
+    
+    
+    def pay_workers(self):
+        return f"You payed your workers. They are happy. Budget left: {left_budget}"
+        return f"You have no budget to pay your workers. They are unhappy"
+    
+    
+    def tend_animals(self):
+        return f"You tended all the animals. They are happy. Budget left: {left_budget}"
+        return f"You have no budget to tend the animals. They are unhappy."
+    
+    
+    def profit(self, amount: int):
+        pass
+    
+    
+    def animals_status(self):
+        pass
