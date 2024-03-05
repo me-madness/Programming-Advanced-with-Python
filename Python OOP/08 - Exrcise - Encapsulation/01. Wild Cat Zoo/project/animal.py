53eab9b9c35d31abc6f3e0a33b4f6e39642595d2
me_madness <p_rangel@abv.bg>
2024-03-05 17:16:49 +0100

    exercise Encapsulation begin

--- Python OOP/08 - Exrcise - Encapsulation/01. Wild Cat Zoo/project/animal.py
+++ Python OOP/08 - Exrcise - Encapsulation/01. Wild Cat Zoo/project/animal.py
@@ -1,3 +1,6 @@
 class Animal:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name: str, gender: str, age: int, money_for_care: int) -> None:
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.money_for_care = money_for_care
