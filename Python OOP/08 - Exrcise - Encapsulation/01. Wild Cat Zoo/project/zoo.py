c7d54c8ddc2ebce8d8288a6317b284ea208f0c8f
me_madness <p_rangel@abv.bg>
2024-03-05 18:49:21 +0100

    Task 01 Wild Cat Zoo is done

--- Python OOP/08 - Exrcise - Encapsulation/01. Wild Cat Zoo/project/zoo.py
+++ Python OOP/08 - Exrcise - Encapsulation/01. Wild Cat Zoo/project/zoo.py
@@ -104,19 +104,3 @@ class Zoo:
             result += f"{v}\n"    
         
         return result
-    
-    
-print(zoo.hire_worker(worker))
-
-# Tending animals
-print(zoo.tend_animals())
-
-# Paying keepers
-print(zoo.pay_workers())
-
-# Fireing worker
-print(zoo.fire_worker("Adam"))
-
-# Printing statuses
-print(zoo.animals_status())
-print(zoo.workers_status())
