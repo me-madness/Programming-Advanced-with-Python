35da5172eb7b49afc98d4c4aadb426becddc04a9
me_madness <p_rangel@abv.bg>
2024-03-05 17:25:09 +0100

    class Worker is done

--- Python OOP/08 - Exrcise - Encapsulation/01. Wild Cat Zoo/project/worker.py
+++ Python OOP/08 - Exrcise - Encapsulation/01. Wild Cat Zoo/project/worker.py
@@ -3,3 +3,7 @@ class Worker:
       self.name = name
       self.age = age
       self.salary = salary
+      
+      
+    def __repr__(self) -> str:
+       return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}"  
