f7984566343e5c97b8e6c21e8e57ef270f99cf09
me_madness <p_rangel@abv.bg>
2024-03-06 17:59:03 +0100

    all most done with second task

--- Python OOP/10 - Static and Class Methods/02. shop.py
+++ Python OOP/10 - Static and Class Methods/02. shop.py
@@ -1,3 +1,26 @@
+from typing import Dict
+
+
 class Shop:
-  
-           
+    def __init__(self, name: str, type_shop: str, capacity: int):
+        self.name = name
+        self.type_shop = type_shop
+        self.capacity = capacity
+        self.items: Dict[str: int] = {}
+        
+        
+    def small_shop(self, name: str, type: str):
+        pass
+    
+    
+    def add_items(self, item_name: str):
+        pass
+    
+    
+    def remove_item(self, item_name: str, amount: int):
+        return f"{amount} {item_name} removed from the shop"
+    return f"Cannot remove {amount} {item_name}"
+    
+    
+    def __repr__(self) -> str:
+       return f"{shop_name} of type {shop_type} with capacity {shop_capacity}"    
