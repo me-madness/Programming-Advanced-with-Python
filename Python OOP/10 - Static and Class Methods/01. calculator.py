353324fc42fcc3914589295826ac1d9a3c9d2621
me_madness <p_rangel@abv.bg>
2024-03-06 17:18:55 +0100

    begin with task 01 Calculator

--- Python OOP/10 - Static and Class Methods/01. calculator.py
+++ Python OOP/10 - Static and Class Methods/01. calculator.py
@@ -1,3 +1,19 @@
 class Calculator:
     def __init__(self) -> None:
         pass
+    
+    
+    def add(*args):
+        return
+    
+    
+    def multiply(*args):
+        return
+    
+    
+    def divide(*args):
+        return
+    
+    
+    def subtract(*args):
+        return
