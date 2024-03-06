02136945cf7f8ee01e3b7656901d2c0c3d384ec2
me_madness <p_rangel@abv.bg>
2024-03-06 18:05:57 +0100

    adding some note to my file

--- Python OOP/10 - Static and Class Methods/02. shop.py
+++ Python OOP/10 - Static and Class Methods/02. shop.py
@@ -1,23 +1,26 @@
 from typing import Dict
-
+# from __future__ import annotations  - this not go correct in Judje system, i can used in my projects
 
 class Shop:
-    def __init__(self, name: str, type_shop: str, capacity: int):
+    def __init__(self, name: str, type_shop: str, capacity: int) -> None:
         self.name = name
         self.type_shop = type_shop
         self.capacity = capacity
         self.items: Dict[str: int] = {}
         
-        
-    def small_shop(self, name: str, type: str):
+    
+    @classmethod    
+    def small_shop(cls, name: str, type_shop: str) -> str: # Here is possible to write the name of class "Shop" not "str"
         pass
     
     
-    def add_items(self, item_name: str):
+    @classmethod
+    def add_items(cls, item_name: str) -> str:
         pass
     
     
-    def remove_item(self, item_name: str, amount: int):
+    @classmethod
+    def remove_item(cls, item_name: str, amount: int) -> None:
         return f"{amount} {item_name} removed from the shop"
     return f"Cannot remove {amount} {item_name}"
     
