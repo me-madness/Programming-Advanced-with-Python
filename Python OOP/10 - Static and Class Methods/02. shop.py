c687344ec4f7c9800f8f0d6272f55f9e2d816c5f
me_madness <p_rangel@abv.bg>
2024-03-06 18:13:00 +0100

    new method is done i my file

--- Python OOP/10 - Static and Class Methods/02. shop.py
+++ Python OOP/10 - Static and Class Methods/02. shop.py
@@ -11,12 +11,16 @@ class Shop:
     
     @classmethod    
     def small_shop(cls, name: str, type_shop: str) -> str: # Here is possible to write the name of class "Shop" not "str"
-        pass
+        return cls(name, type_shop, 10)
     
     
-    @classmethod
-    def add_items(cls, item_name: str) -> str:
-        pass
+    def add_items(self, item_name: str) -> str:
+        if sum(self.items().values()) >= self.capacity:
+            return f"Not enough capacity in the shop"
+        
+        self.items[item_name] = self.items.get(item_name, 0) + 1
+            
+        return f"{item_name} added to the shop"
     
     
     @classmethod
