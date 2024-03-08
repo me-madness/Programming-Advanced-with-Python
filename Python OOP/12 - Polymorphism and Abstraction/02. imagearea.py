0c7d5eb678f002769a96c713f2746ad21239087c
me_madness <p_rangel@abv.bg>
2024-03-08 18:31:43 +0100

    begin with task two

---
+++ Python OOP/12 - Polymorphism and Abstraction/02. imagearea.py
@@ -0,0 +1,8 @@
+class ImageArea:
+    def __init__(self, width: int, height: int) -> None:
+        self.width = width
+        self.height = height
+        
+        
+    def get_area(self):
+        return self.width * self.height    
