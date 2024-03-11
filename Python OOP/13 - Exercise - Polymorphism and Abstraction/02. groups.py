02005e395c11b605efbcee1357b42e62a31de278
me_madness <p_rangel@abv.bg>
2024-03-11 17:47:30 +0100

    second task is done

--- Python OOP/13 - Exercise - Polymorphism and Abstraction/02. groups.py
+++ Python OOP/13 - Exercise - Polymorphism and Abstraction/02. groups.py
@@ -20,3 +20,33 @@ class Group:
       
     def __len__(self):
         return len(self.people)  
+    
+    
+    def __add__(self, other):
+        return Group(f"{self.name} {other.name}", self.people + other.people)
+    
+    
+    def __repr__(self):
+        return f"Group {self.name} with members {', '.join(str(p) for p in self.people)}"
+    
+    
+    def __getitem(self, idx: int):
+        return f"Person {idx}: {str(self.people[idx])}"
+    
+    
+p0 = Person('Aliko', 'Dangote')
+p1 = Person('Bill', 'Gates')
+p2 = Person('Warren', 'Buffet')
+p3 = Person('Elon', 'Musk')
+p4 = p2 + p3
+
+first_group = Group('__VIP__', [p0, p1, p2])
+second_group = Group('Special', [p3, p4])
+third_group = first_group + second_group
+
+print(len(first_group))
+print(second_group)
+print(third_group[0])
+
+for person in third_group:
+    print(person)    
