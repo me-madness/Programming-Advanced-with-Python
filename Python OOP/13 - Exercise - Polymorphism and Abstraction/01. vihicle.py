f4493afc44781dc8c19169aa28d0055af7de5674
me_madness <p_rangel@abv.bg>
2024-03-11 17:25:04 +0100

    adding some data to my file 01. vehicle

--- Python OOP/13 - Exercise - Polymorphism and Abstraction/01. vihicle.py
+++ Python OOP/13 - Exercise - Polymorphism and Abstraction/01. vihicle.py
@@ -1,25 +1,43 @@
 from abc import ABC
 
 class Vehicle(ABC):
-    def __init__(self, fuel_capacity: float, fuel_consumption: float) -> None:
-        self.fuel_capacity = fuel_capacity
+    def __init__(self, fuel_quantity: float, fuel_consumption: float) -> None:
+        self.fuel_quantity = fuel_quantity
         self.fuel_consumption = fuel_consumption
         
         
     @abstractmethod
-    def drive(self, distance: float):
+    def drive(self, distance: float) -> None:
         pass
         
     
     @abstractmethod    
-    def refuel(self, fuel: float):
+    def refuel(self, fuel: float) -> None:
         pass    
             
             
 class Car(Vehicle):
     CONDITIONER_ON: float = 0.9
+    
+    
+    def drive(self, distance: float):
+        consumption = (self.CONDITIONER_ON + self.fuel_consumption) * distance
+        
+        if self.fuel_quantity > consumption:
+            self.fuel_quantity -= consumption
 
+
+    def refuel(self, fuel: float):
+        self.fuel_quantity += fuel
+        
 
 class Truck(Vehicle):
     CONDITIONER_ON: float = 1.6
+    TANK_PERCENTAGE_FILL: float = 0.95
     
+    
+    def drive(self, distance: float):
+        consumption = (self.CONDITIONER_ON + self.fuel_consumption) * distance
+        
+        if self.fuel_quantity > consumption:
+            self.fuel_quantity -= consumption
