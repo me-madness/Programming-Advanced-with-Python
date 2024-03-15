452da96eb4b4e5acf606c191cf74c99e5727f15f
me_madness <p_rangel@abv.bg>
2024-03-15 19:58:57 +0100

    second task is all most done

--- Python OOP/16 - Iterators and Generators/02. reverse_iter.py
+++ Python OOP/16 - Iterators and Generators/02. reverse_iter.py
@@ -1,8 +1,17 @@
 class reverse_iter:
+    def __init__(self, iterable) -> None:
+        self.iterable = iterable
+        self.i = len(self.iterable)
     
     
+    def __iter__(self):
+        return self
     
     
+    def __next__(self):
+        i = self.i
+        self.i += 1
+        return i
     
     
 reversed_list = reverse_iter([1, 2, 3, 4])
