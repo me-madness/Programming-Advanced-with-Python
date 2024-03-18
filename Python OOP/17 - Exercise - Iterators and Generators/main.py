70030c1e34e261fe02d26144bf70acf934b13909
me_madness <p_rangel@abv.bg>
2024-03-18 17:50:17 +0100

    that is done with main.py

--- Python OOP/17 - Exercise - Iterators and Generators/main.py
+++ Python OOP/17 - Exercise - Iterators and Generators/main.py
@@ -20,3 +20,19 @@ class MyList:
 # for loop in python
 
 iterator = MyList([1, 2, 3]).__iter__()
+
+while True:
+    try:
+        print(next(iterator))
+    except StopIteration:
+        break
+    
+    
+def numbers_multiplied_by_two(numbers: list):
+    for number in numbers:
+        yield number * 2
+        
+        
+a = (number * 2 for number in [1, 2, 3])
+print(next(a))                
+print(next(a))                
