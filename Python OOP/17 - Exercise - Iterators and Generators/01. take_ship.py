da68b2a9285d10c85086c88593dda9bd14c124f5
me_madness <p_rangel@abv.bg>
2024-03-18 17:51:34 +0100

    begin with first task

---
+++ Python OOP/17 - Exercise - Iterators and Generators/01. take_ship.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+# First input
+numbers = take_skip(2, 6)
+for number in numbers:
+    print(number)
+
+# Second input
+numbers = take_skip(10, 5)
+for number in numbers:
+    print(number)
