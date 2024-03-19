2422ea2cc8cea0457abd6670caa76a166f0e57b8
me_madness <p_rangel@abv.bg>
2024-03-19 17:12:53 +0100

    adding example for Closure

--- Python OOP/18 - Decorators/main.py
+++ Python OOP/18 - Decorators/main.py
@@ -14,3 +14,14 @@ def hello_function():
 
 hello = hello_function()
 print(hello())    
+
+# Closure
+
+def print_message(message):
+    def message_sender():
+        "Nested Function"
+        print(message)
+    message_sender()
+    
+    
+print_message("Some random message")        
