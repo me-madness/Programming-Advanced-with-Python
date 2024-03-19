97dfa86efbdc6f3629c0fe3d228598a220459b3d
me_madness <p_rangel@abv.bg>
2024-03-19 17:07:30 +0100

    adding same changes in file main.py

--- Python OOP/18 - Decorators/main.py
+++ Python OOP/18 - Decorators/main.py
@@ -1,6 +1,15 @@
 from abc import ABC, abstractmethod
 
-
+@abstractmethod
 class Person(ABC):
     def say_hi(self):
         pass
+    
+    
+def hello_function():
+    def say_hi():
+        return "Hi"
+    return say_hi
+
+
+print(hello_function)    
