2625d5113932d0fa638c4475ccf67dd77b1803fd
me_madness <p_rangel@abv.bg>
2024-03-21 18:37:46 +0100

    the task 05 cache is all most done

--- Python OOP/19 - Exercise - Decorators/05. cashe.py
+++ Python OOP/19 - Exercise - Decorators/05. cashe.py
@@ -1,16 +1,15 @@
+def cache(func):
+    def wrapper(num):
+        if not wrapper.log.get(num):
+            wrapper.log[num] = func(num)
+
+        return wrapper.log[num]
+
+    wrapper.log = {}
 
 
 
-
-
-
-
-
-
-
-
-
-
+@cashe
 def fibonacci(n):
     if n < 2:
         return n
