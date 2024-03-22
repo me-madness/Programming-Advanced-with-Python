9e6762acc2b1767982e973e12675767f7f36eb8b
me_madness <p_rangel@abv.bg>
2024-03-22 20:24:45 +0100

    creating two new classes in main.py

--- Python OOP/20 - Testing/main.py
+++ Python OOP/20 - Testing/main.py
@@ -1,11 +1,20 @@
-import unittest
+from unittest import TestCase, main
 
 class SimpleTest(unittest.TestCase):
+    
     def test_upper(self):
         result = 'foo'.upper()
         expected_result = 'FOO'
         self.assertEqual(result, expected_result)
         
+
+class MyCar:
+    
+    def __init__(self, model: str, hp: int) -> None:
+        self.model = model
+        self.hp = hp
+
+
 
 # Running the test        
 if __name__ == '__main__':
