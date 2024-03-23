13e27e387cade92d217489c40ec9f32f23181776
me_madness <p_rangel@abv.bg>
2024-03-23 14:47:44 +0100

    first test is correct

--- Python OOP/20 - Testing/01. Test Worker/project/test_worker.py
+++ Python OOP/20 - Testing/01. Test Worker/project/test_worker.py
@@ -1,11 +1,18 @@
 from unittest import TestCase, main
 from project.worker import Worker
 
+
 class TestWorker(TestCase):
     
-    def setUp(self):
-        self.worker = Worker()
+    def setUp(self):        # runs before each test case
+        self.worker = Worker("TestGuy", 25_000, 100)
 
+
+    def test_correct_init(self):
+        self.assertEqual("TestGuy", self.worker.name)
+        self.assertEqual(25_000, self.worker.salary)
+        self.assertEqual(100, self.worker.name)
+        self.assertEqual(0, self.worker.money)
 
 
 if __name__ == "__main__":
