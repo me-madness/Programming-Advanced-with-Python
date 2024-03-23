c50612bc2e9de5ea8f63109ca6231295be3b855b
me_madness <p_rangel@abv.bg>
2024-03-23 14:37:33 +0100

    adding the data for task 02  test cat

--- Python OOP/20 - Testing/02. Test Cat/cat.py
+++ Python OOP/20 - Testing/02. Test Cat/cat.py
@@ -4,3 +4,20 @@ class Cat:
         self.fed = False
         self.sleepy = False
         self.size = 0
+        
+      
+    
+    def eat(self):
+        if self.fed:
+            raise Exception('Already fed.')
+
+        self.fed = True
+        self.sleepy = True
+        self.size += 1
+
+    def sleep(self):
+        if not self.fed:
+            raise Exception('Cannot sleep while hungry')
+
+        self.sleepy = False
+        
