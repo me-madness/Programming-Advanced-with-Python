5bd3c0dea506b26348a89aa1f56eb44e0c0a10b2
me_madness <p_rangel@abv.bg>
2024-03-24 13:51:02 +0100

    the third test is ready and work

--- Python OOP/20 - Testing/03. List/test_integer_list.py
+++ Python OOP/20 - Testing/03. List/test_integer_list.py
@@ -11,7 +11,20 @@ class TestIntegerList(TestCase):
         self.assertEqual([1, 2, 3], self.i_list.get_data())
         
         
+    def test_add_non_integer_value_to_the_list_raises_value_error(self):
+        with self.assertRaises(ValueError) as ve:
+            self.i_list.add(5.5)
+            
+        self.assertEqual("Element is not Integer", str(ve.exception))
         
+        
+    def test_add_integer_adds_the_integer_to_the_list(self):
+        expected_list = self.i_list.get_data().copy() + [4]
+        
+        self.i_list.add(4)
+        
+        self.assertEqual(expected_list, self.i_list.get_data())         
+            
         
         
         
