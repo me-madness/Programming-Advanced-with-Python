3e0e018d96c99636a72871b234dc125a6b63bd10
me_madness <p_rangel@abv.bg>
2024-03-24 12:38:00 +0100

    first test is done

--- Python OOP/20 - Testing/02. Test Cat/test_cat.py
+++ Python OOP/20 - Testing/02. Test Cat/test_cat.py
@@ -2,8 +2,20 @@ from unittest import TestCase, main
 from cat import Cat
 
 class TestCat(TestCase):
-    pass
+    
+    def setUp(self):
+        self.cat = Cat("Pancho")
 
+
+    def test_correct_init(self):
+        self.assertEqual("Pancho", self.cat.name)
+        self.assertFalse(self.cat.fed)
+        self.assertFalse(self.cat.sleepy)
+        self.assertEqual(0, self.cat.size)
+
+
+    def test_feed_cat_makes_cat_sleepy_and_not_hungry_expect_size_increase_by_1(self):
+        
 
 
 
