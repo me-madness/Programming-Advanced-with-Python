4badfb349a047156af8b2809ed45fb94802f595b
me_madness <p_rangel@abv.bg>
2024-03-25 17:28:16 +0100

    let start a new task

--- Python OOP/20 - Testing/04. CarManager/test_car_manager.py
+++ Python OOP/20 - Testing/04. CarManager/test_car_manager.py
@@ -1,11 +1,21 @@
 from unittest import TestCase, main
 from car_manager import Car
 
-class TestCarManager(TestCase):
-    pass
+class TestCar(TestCase):
+    
+    def setUp(self):
+        self.car = Car("Nissan", "GT-R", 15, 75)
+        
+        
+    def test_correct_init(self):
+        self.assertEqual("Nissan", self.car.make)    
+        self.assertEqual("GT-R", self.car.model)    
+        self.assertEqual(15, self.car.fuel_consumption)    
+        self.assertEqual(75, self.car.fuel_capacity)    
+        self.assertEqual(0, self.car.fuel_amount)    
 
 
-
+    
 
 
 
