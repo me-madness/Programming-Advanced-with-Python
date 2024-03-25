e924e420dae84c0027c84a1e88bdc304b3c42a16
me_madness <p_rangel@abv.bg>
2024-03-25 18:17:57 +0100

    the first two test for second task are ready

--- Python OOP/21 - Exercise - Testing/02. Vehicle/test/test_vehicle.py
+++ Python OOP/21 - Exercise - Testing/02. Vehicle/test/test_vehicle.py
@@ -5,11 +5,17 @@ from project.vehicle import Vehicle
 class TestVehicle(TestCase):
     
     def setUp(self):
-        self.assertEqual()
-        self.assertEqual()
-        self.assertEqual()
-        self.assertEqual()
-        self.assertEqual()
+        self.vehicle = Vehicle(100, 100)
+        
+        
+    def test_correct_init(self):    
+        self.assertEqual(100, self.vehicle.fuel)
+        self.assertEqual(100, self.vehicle.capacity)
+        self.assertEqual(100, self.vehicle.horse_power)
+        self.assertEqual(self.vehicle.DEFAULT_FUEL_CONSUMPTION, self.vehicle.fuel_consumption)
+    
+    
+    
         
         
         
