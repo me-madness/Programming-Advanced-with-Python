5699918104a3242b0cf20f054bbab5faf636e49d
me_madness <p_rangel@abv.bg>
2024-03-25 18:13:04 +0100

    begin wit second task 02 Vehicle

---
+++ Python OOP/21 - Exercise - Testing/02. Vehicle/test/test_vehicle.py
@@ -0,0 +1,18 @@
+from unittest import TestCase, main
+from project.vehicle import Vehicle
+
+
+class TestVehicle(TestCase):
+    
+    def setUp(self):
+        self.assertEqual()
+        self.assertEqual()
+        self.assertEqual()
+        self.assertEqual()
+        self.assertEqual()
+        
+        
+        
+        
+if __name__ == "__main__":
+    main()        
