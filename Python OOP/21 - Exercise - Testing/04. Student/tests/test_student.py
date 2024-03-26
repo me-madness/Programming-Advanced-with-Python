5520bb1f67adecaabc0a14cbc1c8390192f51dd2
me_madness <p_rangel@abv.bg>
2024-03-26 20:33:09 +0100

    class TestStuden is create in my file test_student.py

--- Python OOP/21 - Exercise - Testing/04. Student/tests/test_student.py
+++ Python OOP/21 - Exercise - Testing/04. Student/tests/test_student.py
@@ -2,3 +2,11 @@ from unittest import TestCase, main
 from project.student import Student
 
 
+class TestStudent(TestCase):
+    
+    def setUp(self):
+        self.student = Student()
+        
+        
+        
+        
