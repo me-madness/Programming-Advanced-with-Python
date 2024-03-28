1ab1084a80b719e843671d47dd0662d40a4ab23a
me_madness <p_rangel@abv.bg>
2024-03-28 17:39:35 +0100

    begin with next file summit_peak.py

---
+++ Python OOP/23 - Exam Preparation/01-Structure-Skeleton/project/peaks/summit_peak.py
@@ -0,0 +1,15 @@
+from peaks.base_peak import BasePeak
+from typing import List
+
+
+class SummitPeak(BasePeak):
+    
+    def get_recommended_gear(self) ->List[str]:
+        return ["Ice axe", "Crampons", "Insulated clothing", "Helmet"]
+    
+    
+    def calculate_difficulty_level(self) -> str:
+        if self.elevation > 3000:
+            return "Extreme"
+        elif 2000 <= self.elevation <= 3000:
+            return "Advanced"     
