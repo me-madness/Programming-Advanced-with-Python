a13040078e115b2a0e22ef53062e8dde43b4144b
me_madness <p_rangel@abv.bg>
2024-03-29 17:59:52 +0100

    lets finish the property and setter function

--- Python OOP/24 - Exam Preparation/01-Structure-Skeleton/project/divers/base_diver.py
+++ Python OOP/24 - Exam Preparation/01-Structure-Skeleton/project/divers/base_diver.py
@@ -1,5 +1,36 @@
+from abc import ABC, abstractmethod
+
+
 class BaseDiver:
     
     def __init__(self, name: str, oxygen_level: float) -> None:
         self.name = name
         self.oxygen_level = oxygen_level
+        self.catch: list = []
+        
+        
+    @property
+    def name(self):
+        return self.__name
+    
+    
+    @name.setter
+    def name(self, value) :
+        if value.strip() == "":
+            raise ValueError("Diver name cannot be null or empty!")           
+        self.__name = value
+        
+        
+    @property
+    def oxygen_level(self):
+        return self.__oxygen_level
+    
+    
+    @oxygen_level.setter
+    def oxygen_level(self, value) :
+        pass           
+        self.__oxygen_level = value
+        
+        
+            
+    
