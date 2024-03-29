c1fd147f51626af165c2f24b8df3b44c81f4a648
me_madness <p_rangel@abv.bg>
2024-03-29 17:22:44 +0100

    start with first class BaseFish

--- Python OOP/24 - Exam Preparation/01-Structure-Skeleton/project/fish/base_fish.py
+++ Python OOP/24 - Exam Preparation/01-Structure-Skeleton/project/fish/base_fish.py
@@ -1,4 +1,9 @@
+from abc import ABC, abstractmethod
+
+
 class BaseFish:
     
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, point: float, time_to_catch: int) -> None:
         self.name = name
+        self.point = point
+        self.time_to_catch = time_to_catch
