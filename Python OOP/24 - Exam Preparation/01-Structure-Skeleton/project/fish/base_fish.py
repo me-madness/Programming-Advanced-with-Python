b906b9cf3fc0922f3dad04b2c5dae8a2fe9a712a
me_madness <p_rangel@abv.bg>
2024-03-29 17:27:15 +0100

    lets begin with abstarctmethod

--- Python OOP/24 - Exam Preparation/01-Structure-Skeleton/project/fish/base_fish.py
+++ Python OOP/24 - Exam Preparation/01-Structure-Skeleton/project/fish/base_fish.py
@@ -7,3 +7,33 @@ class BaseFish:
         self.name = name
         self.point = point
         self.time_to_catch = time_to_catch
+        
+        
+    @property
+    def name(self):
+        return
+    
+    
+    @name.setter
+    def name(self, value):
+        pass  
+    
+    
+    @property
+    def point(self):
+        pass
+    
+    
+    @point.setter
+    def point(self, value):
+        pass 
+    
+    
+    @property
+    def time_to_catch(self):
+        pass
+    
+    
+    @time_to_catch.setter
+    def time_to_catch(self, value):
+        pass   
