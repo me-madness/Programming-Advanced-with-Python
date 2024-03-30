20cb10ef0672e0df73164b998045606a62803c3b
me_madness <p_rangel@abv.bg>
2024-03-30 17:40:54 +0100

    i create a new function swim_into_competition

--- Python OOP/24 - Exam Preparation/01-Structure-Skeleton/project/nautical_catch_challenge_app.py
+++ Python OOP/24 - Exam Preparation/01-Structure-Skeleton/project/nautical_catch_challenge_app.py
@@ -1,11 +1,12 @@
 from fish.base_fish import BaseFish
 from divers.base_diver import BaseDiver
-from divers.free_diver import FreeDivers
+from divers.free_diver import FreeDiver
 
 from typing import List
 
 
 class NauticalCatchChallengeApp:
+    divers_mapper = {"FreeDiver": FreeDiver, "ScubaDiver": ScubaDiver}
     
     def __init__(self) -> None:
         self.divers: List[BaseDiver] = []
@@ -13,4 +14,8 @@ class NauticalCatchChallengeApp:
         
         
     def dive_into_competition(diver_type: str, diver_name: str):
-        pass    
+        pass  
+    
+    
+    def swim_into_competition(self, fish_type: str, fish_name: str, points: float):
+        pass  
