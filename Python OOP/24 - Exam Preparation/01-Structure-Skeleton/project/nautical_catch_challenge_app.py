f32c50efee61183f68492e4b7d78046da1c7c404
me_madness <p_rangel@abv.bg>
2024-03-30 17:46:46 +0100

    all most is done my def dive_into_competition

--- Python OOP/24 - Exam Preparation/01-Structure-Skeleton/project/nautical_catch_challenge_app.py
+++ Python OOP/24 - Exam Preparation/01-Structure-Skeleton/project/nautical_catch_challenge_app.py
@@ -1,6 +1,7 @@
 from fish.base_fish import BaseFish
 from divers.base_diver import BaseDiver
 from divers.free_diver import FreeDiver
+from divers.scuba_diver import ScubaDiver
 
 from typing import List
 
@@ -13,9 +14,19 @@ class NauticalCatchChallengeApp:
         self.fish: List[BaseFish] = []
         
         
-    def dive_into_competition(diver_type: str, diver_name: str):
-        pass  
+    def dive_into_competition(self, diver_type: str, diver_name: str):
+        if diver_type not in self.divers_mapper:  
+            return f"{diver_type} is not allowed in our competition."
     
+        try:
+            diver = [d for d in self.divers if d.name == diver_name][0]
+            return f"{diver_name} is already a participant."
+        except IndexError:
+                
     
     def swim_into_competition(self, fish_type: str, fish_name: str, points: float):
         pass  
+    
+    
+    def chase_fish(self, diver_name: str, fish_name: str, is_lucky: bool):
+        pass
