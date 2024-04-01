1a792f55130012945ff87f5ce2aff1c63fddbe3b
me_madness <p_rangel@abv.bg>
2024-04-01 17:08:45 +0200

    begin with workshop class

--- Python OOP/25 - Workshop/hashtable.py
+++ Python OOP/25 - Workshop/hashtable.py
@@ -1,4 +1,16 @@
 class HashTable:
     
     def __init__(self) -> None:
-        pass
+        self.array = [None, None, None, None]
+    
+    
+    
+table = HashTable()
+
+table["name"] = "Peter"
+table["age"] = 25
+
+print(table)
+print(table.get("name"))
+print(table["age"])
+print(len(table))    
