7eaa4d2ce65ebb0f5e7d707ff9f42ef8b1678a5a
me_madness <p_rangel@abv.bg>
2024-04-02 19:18:42 +0200

    def test_init( is create

--- Python OOP/25 - Workshop/test_hashtable.py
+++ Python OOP/25 - Workshop/test_hashtable.py
@@ -7,6 +7,10 @@ class TestHashTable(TestCase):
         self.h = HashTable()
     
     
+    def test_init(self):
+        self.assertEqual(self.h_Hashtable__keys, [None, None, None, None])
+        self.assertEqual(self.h_Hashtable__values, [None, None, None, None])
+        self.assertEqual(self.h_Hashtable__length, 4)
     
     
 if __name__ == "__main__":
