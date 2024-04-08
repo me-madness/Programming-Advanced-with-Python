f3899beda8d9240fc0ffa3095a98e1d857d89757
me_madness <p_rangel@abv.bg>
2024-04-08 21:09:43 +0200

    one more changes in my test file

---
+++ Python OOP/Regular Exam/Testing-Influencer Manager App/project/test/test.py
@@ -0,0 +1,16 @@
+from unittest import TestCase, main
+
+
+
+class TestInfluencerManagerApp:
+    
+    def setUp(self):
+        pass
+    
+    
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
